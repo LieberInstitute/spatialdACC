8dc9ab885267949c87fe8be59b136224632da020
Ryan Miller <rmiller@compute-105.cm.cluster>
2023-03-29 10:18:29 -0400

    added splitSlide.sh bash script

---
+++ code/VistoSeg/code/splitSlide.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=100G,h_vmem=100G,h_stack=256M,h_fsize=100G
+#$ -o /dcs04/lieber/marmaypag/Visium_troubleshooting_LIBD001/VistoSeg/code/logs/$TASK_ID_splitSlide.txt 
+#$ -e /dcs04/lieber/marmaypag/Visium_troubleshooting_LIBD001/VistoSeg/code/logs/$TASK_ID_splitSlide.txt
+#$ -m e
+#$ -M heenadivecha@gmail.com
+#$ -t 5
+#$ -tc 1
+
+echo "**** Job starts ****"
+date
+
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"s
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## load MATLAB
+module load matlab/R2019a
+
+## Load toolbox for VistoSeg
+toolbox='/dcs04/lieber/marmaypag/Visium_troubleshooting_LIBD001/VistoSeg/code'
+
+## Read inputs from splitSlide_list.txt file
+fname=$(awk 'BEGIN {FS="\t"} {print $1}' splitSlide_list.txt | awk "NR==${SGE_TASK_ID}")
+A1=$(awk 'BEGIN {FS="\t"} {print $2}' splitSlide_list.txt | awk "NR==${SGE_TASK_ID}")
+B1=$(awk 'BEGIN {FS="\t"} {print $3}' splitSlide_list.txt | awk "NR==${SGE_TASK_ID}")
+C1=$(awk 'BEGIN {FS="\t"} {print $4}' splitSlide_list.txt | awk "NR==${SGE_TASK_ID}")
+D1=$(awk 'BEGIN {FS="\t"} {print $5}' splitSlide_list.txt | awk "NR==${SGE_TASK_ID}")
+
+## Run refineVNS function
+matlab -nodesktop -nosplash -nojvm -r "addpath(genpath('$toolbox')), splitSlide('$fname',$A1,$B1,$C1,$D1)"
+
+echo "**** Job ends ****"
+date
+
+
+
