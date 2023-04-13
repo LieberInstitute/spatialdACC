46b8dcd9ef093eb14bc57d47c008c02e30241b84
Ryan Miller <rmiller@jhpce01.cm.cluster>
2023-04-13 17:01:14 -0400

    shell script to build spe object and logs

---
+++ code/02_build_spe/01_raw_spe.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -l mem_free=8G,h_vmem=8G,h_fsize=100G
+#$ -N spatialdACC_build_spe
+#$ -o logs/raw_spe.$TASK_ID.txt
+#$ -e logs/raw_spe.$TASK_ID.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R/4.2.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript -e 01_raw_spe.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
