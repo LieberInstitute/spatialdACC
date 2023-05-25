38d7523c16720936ba2c067285c8403dd6f0fa46
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2023-05-25 15:37:05 -0400

    spatialLIBD shinyapp is now available to exlpore :)

---
+++ code/spatialLIBD_app/app.R
@@ -0,0 +1,26 @@
+library("spatialLIBD")
+library("markdown") ## due to a shinyapps.io bug
+
+## spatialLIBD uses golem
+options("golem.app.prod" = TRUE)
+
+## You need this to enable shinyapps to install Bioconductor packages
+options(repos = BiocManager::repositories())
+
+## Load the data (all paths are relative to this script's location)
+spe <- load("spe_raw.Rdata")
+# spe$CellCount <- spe$segmentation_info
+# spe$CellCount <- spe$NBW
+vars <- colnames(colData(spe))
+
+## Deploy the website
+spatialLIBD::run_app(
+    spe,
+    sce_layer = NULL,
+    modeling_results = NULL,
+    sig_genes = NULL,
+    title = "spatial_dACC, Visium",
+    spe_discrete_vars = c(vars[grep("10x_", vars)], "ManualAnnotation"),
+    spe_continuous_vars = c("sum_umi", "sum_gene", "expr_chrM", "expr_chrM_ratio"),
+    default_cluster = "10x_graphclust"
+)
