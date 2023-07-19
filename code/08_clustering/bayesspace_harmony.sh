aef1a454c3a7c966ef133e430404222583ff3ae7
kinnaryshah <kinnaryshahh@gmail.com>
2023-07-19 15:41:39 -0400

    reduce memory, extra text due to errors

--- code/08_clustering/bayesspace_harmony.sh
+++ code/08_clustering/bayesspace_harmony.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 #$ -cwd
-#$ -l caracol,mem_free=120G,h_vmem=120G,h_fsize=100G
+#$ -l mem_free=30G,h_vmem=30G,h_fsize=30G
 #$ -N bayesSpace_captureArea_k_many
 #$ -o logs_bayesspace_harmony/bayesSpace_captureArea_k.$TASK_ID.txt
 #$ -e logs_bayesspace_harmony/bayesSpace_captureArea_k.$TASK_ID.txt
 #$ -m e
 #$ -t 10-11
 #$ -tc 2
-
-
-USAGE_CUTOFF=10
-NUM_GPUS=1
-
-avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
-
-#  Simply exit with an error if there are no GPUs left
-if [[ -z $avail_gpus ]]; then
-    echo "No GPUs are available."
-    exit 1
-fi
-
-export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
-
 
 echo "**** Job starts ****"
 date
