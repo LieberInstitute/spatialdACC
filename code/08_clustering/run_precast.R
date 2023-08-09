5595b3dcb95b45a8c507f9d226be65b056b0e5c6
Kinnary Shah <kinnaryshahh@gmail.com>
2023-08-09 12:04:17 -0400

    create seurat obj per brnum then run PRECAST

--- code/08_clustering/run_precast.R
+++ code/08_clustering/run_precast.R
@@ -13,23 +13,34 @@ suppressPackageStartupMessages({
 #start from spe without batch correction
 load(here("processed-data", "06_preprocessing", "spe_dimred.Rdata"))
 
-spe$spot_id = spe$key
+spe_to_seuratList <- function(spe){
+    uniq_brnum <- colData(spe)$brnum |> unique()
 
-spe$row <- spe$array_row
-spe$col <- spe$array_col
+    # Create a seurat object for each unique brnum
+    map(uniq_brnum,
+        .f = function(bnum, spe){
+            ret_spe <- spe[, colData(spe)$brnum == bnum]
+            ret_seurat <- spe_to_seurat(ret_spe)
 
-seu <- CreateSeuratObject(
-    counts=as.matrix(counts(spe)),
-    meta.data=data.frame(colData(spe)),
-    project="dACC")
-
-seuList = list()
-
-brains = unique(spe$brnum)
-
-for (i in seq_along(brains)){
-    seuList[[i]] = subset(x=seu, subset = brnum == brains[i])
+            return(ret_seurat)
+        },
+        spe = spe)
 }
+
+spe_to_seurat <- function(spe){
+
+    ret <- CreateSeuratObject(
+        counts=assays(spe)$counts,
+        meta.data=data.frame(
+            row=spatialCoords(spe)[,1],
+            col=spatialCoords(spe)[,2]),
+        spot_id = colData(spe_sub)$spot_id
+    )
+
+    return(ret)
+}
+
+seuList <- spe_to_seuratList(spe)
 
 preobj = CreatePRECASTObject(seuList = seuList, gene.number=2000, selectGenesMethod='HVGs')
 preobj@seulist
