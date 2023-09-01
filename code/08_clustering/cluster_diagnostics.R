7d4ec277f2ccd6d55deead427dc3bd78a27ebff0
Kinnary Shah <kinnaryshahh@gmail.com>
2023-09-01 11:17:08 -0400

    run cluster diagnostics & create boxplots

---
+++ code/08_clustering/cluster_diagnostics.R
@@ -0,0 +1,132 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+suppressPackageStartupMessages({
+    library(dplyr)
+    library(purrr)
+    library(Seurat)
+    library(SpatialExperiment)
+    library(PRECAST)
+    library(spatialLIBD)
+    library(ggplot2)
+    library(gridExtra)
+    library(here)
+    library(bluster)
+})
+
+read_barcoded_csv <- function(x) {
+    df <- read.csv(x)
+    colnames(df) <- tolower(colnames(df))
+
+    if (colnames(df)[2] == "cluster") {
+        colnames(df)[2] <-
+            gsub("gene_expression_", "", basename(dirname(x)))
+    }
+    return(df)
+}
+
+PRECAST_import <- function(spe, cluster_dir = file.path(tempdir(), "exported_clusters")) {
+    clustering_files <-
+        list.files(
+            here::here("processed-data", "08_clustering", "PRECAST", precast_name),
+            pattern = "clusters.csv",
+            all.files = TRUE,
+            full.names = TRUE,
+            recursive = TRUE
+        )
+    clusters_list <- lapply(clustering_files, read_barcoded_csv)
+    clusters <- Reduce(function(...) merge(..., by = "key", all = TRUE), clusters_list)
+    cluster_cols <- which(colnames(clusters) != "key")
+    colnames(clusters)[cluster_cols] <- paste0("", colnames(clusters)[cluster_cols])
+
+    colData(spe)[,precast_name] <- clusters[,2]
+    return(spe)
+}
+
+Sys.time()
+
+load(here("processed-data", "06_preprocessing", "spe_dimred.Rdata"))
+
+#import cluster columns into single spe
+num_clusters <- c(5:20)
+for (k in num_clusters) {
+
+    #import bayesspace harmony clusters
+    bayesSpace_name <- paste0("bayesSpace_captureArea_", k)
+    spe <- cluster_import(
+        spe,
+        cluster_dir = here::here("processed-data", "08_clustering", "BayesSpace", "preprocess_harmony", bayesSpace_name),
+        prefix = "harmony_"
+    )
+
+    #import bayesspace mnn clusters
+    spe <- cluster_import(
+        spe,
+        cluster_dir = here::here("processed-data", "08_clustering", "BayesSpace", "preprocess_mnn", bayesSpace_name),
+        prefix = "mnn_"
+    )
+
+    #import precast clusters
+    #i re wrote the key when using PRECAST, just add the cluster columns manually
+    precast_name <- paste0("PRECAST_captureArea_", k)
+    spe <- PRECAST_import(
+        spe,
+        cluster_dir = here::here("processed-data", "08_clustering", "PRECAST", precast_name)
+    )
+}
+
+Sys.time()
+
+#find 13 spots that PRECAST filtered out
+non_na_indices <- !is.na(colData(spe)$PRECAST_captureArea_5)
+
+clustering_columns <- colData(spe)[,c(49:96)]
+column_names <- colnames(colData(spe))[c(49:96)]
+
+purity.data.list <- list()
+
+i<-1
+
+for (i in seq_along(clustering_columns)) {
+    current_clustering <- clustering_columns[, i]
+    current_colname <- column_names[i]
+
+    # Remove NAs from the current_clustering
+    current_clustering_no_na <- current_clustering[non_na_indices]
+    reduced_dim_no_na <- reducedDim(spe, "pp-GLM-PCA")[non_na_indices,]
+
+    purity.approx <- neighborPurity(reduced_dim_no_na, clusters = current_clustering_no_na)
+    purity.data <- as.data.frame(purity.approx)
+    purity.data$maximum <- factor(purity.data$maximum)
+    purity.data$cluster <- current_clustering_no_na
+
+    purity.data.list[[current_colname]] <- purity.data
+}
+
+avg_purity <- data.frame(clustering = character(), cluster = integer(), avg_purity = numeric())
+for (colname in names(purity.data.list)) {
+    purity.data <- purity.data.list[[colname]]
+    avg_purity_col <- aggregate(purity.data$purity, by = list(purity.data$cluster), FUN = mean)
+    colnames(avg_purity_col) <- c("cluster", "avg_purity")
+    avg_purity_col$clustering <- colname
+    avg_purity <- rbind(avg_purity, avg_purity_col)
+}
+
+avg_purity$clustering <- factor(avg_purity$clustering, levels = names(purity.data.list))
+# Add a column for the algorithm
+avg_purity$algorithm <- factor(ifelse(grepl("harmony", avg_purity$clustering), "Harmony - BS",
+                                      ifelse(grepl("PRECAST", avg_purity$clustering), "PRECAST",
+                                             "MNN - BS")))
+
+save(avg_purity,file=here::here("plots","08_clustering","cluster_diagnostics","purity_boxplot.rda"))
+
+
+boxplot <- ggplot(avg_purity, aes(x = clustering, y = avg_purity,fill=algorithm)) +
+    geom_boxplot() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+    #facet_wrap(~ algorithm, strip.position = "bottom")+
+    scale_fill_manual(values = c("Harmony - BS" = "#f88379", "PRECAST" = "#afeeee",
+                                 "MNN - BS" = "#f9e09c"))+
+    ggtitle("Average cluster purity for different clusterings")
+
+pdf(here::here('plots', '08_clustering', 'cluster_diagnostics','avg_purity_boxplot.pdf'))
+print(boxplot)
+dev.off()
