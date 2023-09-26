e0f181ed6c290c55318817e64732a79ba479e534
kinnaryshah <kinnaryshahh@gmail.com>
2023-09-26 10:59:24 -0400

    fix platform argument, add row/col

--- code/08_clustering/qtune.R
+++ code/08_clustering/qtune.R
@@ -12,16 +12,24 @@ suppressPackageStartupMessages({
 })
 
 load(here("processed-data", "07_batch_correction", "spe_harmony.Rdata"))
+colData(spe)$row <- spe$array_row
+colData(spe)$col <- spe$array_col
+
+metadata(spe)$BayesSpace.data <- list(platform = "Visium", is.enhanced = FALSE)
 spe_harmony <- spe
 dim(spe_harmony)
 
-spe_harmony <- qTune(spe_harmony, qs=seq(5, 20), platform="ST")
+spe_harmony <- qTune(spe_harmony, use.dimred = "HARMONY", qs=seq(5, 20))
 
 load(here("processed-data", "07_batch_correction", "spe_mnn.Rdata"))
+colData(spe)$row <- spe$array_row
+colData(spe)$col <- spe$array_col
+
+metadata(spe)$BayesSpace.data <- list(platform = "Visium", is.enhanced = FALSE)
 spe_mnn <- spe
 dim(spe_mnn)
 
-spe_mnn <- qTune(spe_mnn, qs=seq(5, 20), platform="ST")
+spe_mnn <- qTune(spe_mnn, use.dimred = "MNN", qs=seq(5, 20))
 
 pdf(file = here::here("plots", "08_clustering", "BayesSpace", "qtune.pdf"), width = 21, height = 20)
 qPlot(spe_harmony)
