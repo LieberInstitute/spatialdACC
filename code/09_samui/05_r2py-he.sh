38194338efa0799c576ac42793c9fa4fe7907f87
Ryan Miller <ry.a.miller3@gmail.com>
2023-10-05 09:52:45 -0400

    updates to shell scripts for samui

---
+++ code/09_samui/05_r2py-he.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --mem=50G
+#SBATCH --job-name=r2python
+#SBATCH --array=1
+
+echo -en '\n'
+echo "**** Job starts ****"
+date
+echo -en '\n'
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3
+Rscript 01_r2py.R
+
+mkdir -p ./logs/
+
+cat ./slurm-${SLURM_JOBID}*.out >> ./logs/01_r2py.txt
+echo -en '\n'
+rm ./slurm-${SLURM_JOBID}*.out
+
+echo "**** Job ends ****"
+date
