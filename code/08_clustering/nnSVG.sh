ab9754721a392a3b5ae1b510be9819a823c56329
kinnaryshah <kinnaryshahh@gmail.com>
2023-10-06 10:21:29 -0400

    change memory/threads

--- code/08_clustering/nnSVG.sh
+++ code/08_clustering/nnSVG.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-#$ -cwd
-#$ -l mem_free=30,h_vmem=30,h_fsize=100G
-#$ -pe local 12
-#$ -N nnSVG
-#$ -o logs_nnSVG/nnSVG_1000.txt
-#$ -e logs_nnSVG/nnSVG_1000.txt
-#$ -m e
-
+#SBATCH --job-name=nnSVG
+#SBATCH --output=logs_nnSVG/nnSVG.txt
+#SBATCH --error=logs_nnSVG/nnSVG.txt
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=20G
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com # Please replace with the appropriate email address
 echo "**** Job starts ****"
 date
-
-echo "**** JHPCE info ****"
+echo "**** SLURM info ****"
 echo "User: ${USER}"
-echo "Job id: ${JOB_ID}"
-echo "Job name: ${JOB_NAME}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SGE_TASK_ID}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
 
 module load conda_R/devel
 
