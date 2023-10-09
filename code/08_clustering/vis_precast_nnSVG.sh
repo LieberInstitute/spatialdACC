9abb55290d90568cc0f8eb8fe4ca2837a94e70aa
Kinnary Shah <kinnaryshahh@gmail.com>
2023-10-09 10:08:33 -0400

    visualize nnSVG PRECAST clusters

---
+++ code/08_clustering/vis_precast_nnSVG.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=nnSVG_PRECAST
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10G
+#SBATCH --output=logs_precast/vis_nnsvg_PRECAST_batch_k.%a.txt
+#SBATCH --error=logs_precast/vis_nnsvg_PRECAST_batch_k.%a.txt
+#SBATCH --array=5-20%5
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript vis_precast_nnSVG.R
+
+echo "**** Job ends ****"
+date
