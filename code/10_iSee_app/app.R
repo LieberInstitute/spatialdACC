903e60cbdee521e8d3bf2a0b4e134ee1197d540f
rmill162 <rmill162@eb-li-qpmy24h3.win.ad.jhu.edu>
2023-10-17 13:00:26 -0400

    update and troubleshooting isee app

--- code/10_iSee_app/app.R
+++ code/10_iSee_app/app.R
@@ -4,41 +4,43 @@ library("shiny")
 library("paletteer")
 
 load("1c-10c_sce_raw.rda", verbose = TRUE)
+#load("sce_for_iSEE_LS.rda", verbose = TRUE)
 
-stopifnot(all(unique(sce.ls.small$cellType.final) %in% names(cell_cols.clean)))
+#stopifnot(all(unique(sce$cellType.final) %in% names(cell_cols.clean)))
 
 ## Don't run this on app.R since we don't want to run this every single time
-# lobstr::obj_size(sce.ls.small)
+# lobstr::obj_size(sce)˚
 # 876.34 MB
 
-source("initial.R", print.eval = TRUE)
+#source("initial.R", print.eval = TRUE)
 
 ## From https://github.com/LieberInstitute/10xPilot_snRNAseq-human/blob/810b47364af4c8afe426bd2a6b559bd6a9f1cc98/shiny_apps/tran2021_AMY/app.R#L10-L14
 ## Related to https://github.com/iSEE/iSEE/issues/568
-colData(sce.ls.small) <- cbind(
-    colData(sce.ls.small)[, !colnames(colData(sce.ls.small)) %in% c("Sample", "cellType.final")],
-    colData(sce.ls.small)[, c("cellType.final", "Sample")]
+colData(sce) <- cbind(
+    colData(sce)[, !colnames(colData(sce)) %in% c("Sample", "round")],
+    colData(sce)[, c("round", "Sample")]
 )
 
-sce.ls.small$Sample <- as.factor(sce.ls.small$Sample)
+sce$Sample <- as.factor(sce$Sample)
 
-sce.ls.small <- registerAppOptions(sce.ls.small, color.maxlevels = length(cell_cols.clean))
+#sce <- registerAppOptions(sce, color.maxlevels = length(cell_cols.clean))
 iSEE(
-    sce.ls.small,
-    appTitle = "snRNAseq_lateral_septum",
-    initial = initial,
+    sce,
+    appTitle = "snRNAseq_dACC",
+    #initial = initial,
     colormap = ExperimentColorMap(colData = list(
         Sample = function(n) {
             cols <- paletteer::paletteer_d(
                 palette = "RColorBrewer::Dark2",
-                n = length(unique(sce.ls.small$Sample))
+                n = length(unique(sce$Sample))
             )
             cols <- as.vector(cols)
-            names(cols) <- levels(sce.ls.small$Sample)
+            names(cols) <- levels(sce$Sample)
             return(cols)
-        },
-        cellType.final = function(n) {
-            return(cell_cols.clean)
         }
+        # ,
+        # cellType.final = function(n) {
+        #     return(cell_cols.clean)
+        # }
     ))
 )
