a99f452b421a0350b52bd7860f6fda45983b8700
Ryan Miller <ry.a.miller3@gmail.com>
2023-11-27 10:03:46 -0500

    re-ran samui for IF images using add_chunked_feature funtion. cleaned up code repository for samui. made new files for generating samui output for H&E stained samples

---
+++ code/09_samui/04_loopy-he.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --mem=10G
+#SBATCH --job-name=04samui
+#SBATCH --output=logs/%x%a.txt
+#SBATCH --array=1-4%4
+
+# #SBATCH --error=logs/%x%a.txt
+
+#echo -e "\n"
+echo "date: $(date)"
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_NODENAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load samui/1.0.0-next.45
+python 03_loopy-he.py
+
+echo "**** Job ends ****"
+date
+
+#echo -e "\n"
