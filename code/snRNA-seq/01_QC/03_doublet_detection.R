edbe159e780c9ff52270d945f5604c55fe2973db
Kinnary Shah <kinnaryshahh@gmail.com>
2024-01-25 11:57:51 -0500

    re run doublet detection after removing nuclei

--- code/snRNA-seq/01_QC/03_doublet_detection.R
+++ code/snRNA-seq/01_QC/03_doublet_detection.R
@@ -9,47 +9,40 @@ library("tidyverse")
 
 load(here("processed-data", "snRNA-seq", "01_QC", "sce_qc.rda"))
 
-sce <- scDblFinder(sce, samples="Sample", BPPARAM=MulticoreParam(8,RNGseed=1234))
+sce <- scDblFinder(sce, samples="Sample", BPPARAM=MulticoreParam(2,RNGseed=1234))
 
 #save doublet scores sce
 save(sce, file=here("processed-data", "snRNA-seq", "01_QC", "sce_doublet.rda"))
 
 summary(sce$scDblFinder.score)
 #      Min.   1st Qu.    Median      Mean   3rd Qu.      Max.
-# 0.0002785 0.0006567 0.0010397 0.0644836 0.0031831 0.9997794
+# 0.0000381 0.0004016 0.0009069 0.0609606 0.0018350 0.9999698
+
+table(sce$scDblFinder.class)
+# singlet doublet
+# 39849    2325
 
 table(sce$Sample, sce$scDblFinder.class)
 #               singlet doublet
-#  10c_dACC_SVB    5312     257
-# 1c_dACC_MRV     4206     229
-# 2c_dACC_MRV     4406     225
-# 3c_dACC_MRV     4068     260
-# 4c_dACC_MRV     4035     345
-# 5c_dACC_SVB     2606     159
-# 6c_dACC_SVB     2836     160
-# 7c_dACC_SVB     4218     210
-# 8c_dACC_SVB     3941     239
-# 9c_dACC_SVB     4658     394
+#  10c_dACC_SVB    5109     246
+# 1c_dACC_MRV     4186     241
+# 2c_dACC_MRV     4329     238
+# 3c_dACC_MRV     4084     229
+# 4c_dACC_MRV     4022     269
+# 5c_dACC_SVB     2631     131
+# 6c_dACC_SVB     2680     158
+# 7c_dACC_SVB     4216     178
+# 8c_dACC_SVB     3946     232
+# 9c_dACC_SVB     4646     403
 
 sum(sce$scDblFinder.score > 0.99)
-# [1] 1745
+# [1] 1666
 
 # check if doublets are overlapping with low qc
 #most of the high mito are singlets
-#none of the low sum/detected are doublets
 table(sce$high_mito, sce$scDblFinder.class)
 #        singlet doublet
-# FALSE   34831    2288
-# TRUE     5455     190
-
-table(sce$low_sum, sce$scDblFinder.class)
-#         singlet doublet
-# FALSE   40002    2478
-# TRUE      284       0
-
-table(sce$low_detected, sce$scDblFinder.class)
-#         singlet doublet
-# FALSE   39930    2478
-# TRUE      356       0
+# FALSE   34876    2156
+# TRUE     4973     169
 
 #we just flagged doublet scores, did not remove from sce
