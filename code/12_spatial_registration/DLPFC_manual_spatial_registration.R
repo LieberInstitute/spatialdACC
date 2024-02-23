a26c4ac489969858c13666802d80a8f0962fba49
Kinnary Shah <kinnaryshahh@gmail.com>
2024-02-23 08:53:28 -0500

    DLPFC manual spatial reg

---
+++ code/12_spatial_registration/DLPFC_manual_spatial_registration.R
@@ -0,0 +1,57 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+
+library("here")
+library("spatialLIBD")
+library("SingleCellExperiment")
+library("stringr")
+
+# get reference layer enrichment statistics
+k <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+nnSVG_precast_name <- paste0("nnSVG_PRECAST_captureArea_", k)
+load(file = here("processed-data", "11_differential_expression", "pseudobulk",
+                 "nnSVG_precast_DE", paste0(nnSVG_precast_name,".Rdata")))
+
+# load DLPFC manual annotations
+spe <- spatialLIBD::fetch_data(type = "spe")
+table(spe$sample_id, spe$layer_guess_reordered)
+
+spe_modeling_results <- registration_wrapper(
+    spe,
+    var_registration = "layer_guess_reordered",
+    var_sample_id = "sample_id",
+    gene_ensembl = "gene_id",
+    gene_name = "gene_name"
+)
+
+## extract t-statics and rename
+registration_t_stats <- spe_modeling_results$enrichment[, grep("^t_stat", colnames(spe_modeling_results$enrichment))]
+colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_stats))
+
+## cell types x gene
+dim(registration_t_stats)
+
+## check out table
+registration_t_stats[1:5, 1:5]
+
+cor_layer <- layer_stat_cor(
+    stats = registration_t_stats,
+    modeling_results = modeling_results,
+    model_type = "enrichment",
+    top_n = 100
+)
+
+cor_layer
+save(cor_layer, file = here("processed-data", "12_spatial_registration",paste0("DLPFC_manual_",nnSVG_precast_name,".rds")))
+
+pdf(file = here::here("plots", "12_spatial_registration", "DLPFC_manual",
+                      paste0("DLPFC_manual_",nnSVG_precast_name,"_heatmap.pdf")), width = 14, height = 14)
+layer_stat_cor_plot(cor_layer, max = max(cor_layer))
+dev.off()
+
+anno <- annotate_registered_clusters(
+    cor_stats_layer = cor_layer,
+    confidence_threshold = 0.25,
+    cutoff_merge_ratio = 0.25
+)
+
+anno
