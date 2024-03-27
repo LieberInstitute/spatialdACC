7be06cbc250592c3071149791a982138a107d492
Kinnary Shah <kinnaryshahh@gmail.com>
2024-03-27 17:32:18 -0400

    correlation with qc metrics

--- code/snRNA-seq/06_NMF/02_technical_var_correlation.R
+++ code/snRNA-seq/06_NMF/02_technical_var_correlation.R
@@ -9,7 +9,7 @@ library(reshape2)
 load(file = here("processed-data", "snRNA-seq", "03_batch_correction", "sce_harmony.Rdata"))
 x <- readRDS(file = here("processed-data", "snRNA-seq", "06_NMF", "nmf_results.RDS"))
 
-####onehot encode both brain as an example
+####onehot encode brain
 data<-as.data.frame(sce$brain)
 colnames(data)<-'brain'
 onehot_brain <-  dcast(data = data, rownames(data) ~ brain, length)
@@ -19,12 +19,23 @@ onehot_brain<-onehot_brain[order(onehot_brain[,1],decreasing=F),]
 onehot_brain[,1]<-NULL
 
 ###correlate with nmf patterns
-pheatmap(cor(t(x@h),onehot_brain))
+pdf(here("plots", "snRNA-seq", "06_NMF", "nmf_brain_correlation_heatmap.pdf"))
+pheatmap(cor(t(x@h),onehot_brain), fontsize_row = 5)
+dev.off()
 
 ###repeat with any categorical vars
 
 ###for continuous tech vars do this:
 
 vars<-colData(sce)[,c('detected','sum','subsets_Mito_percent')]
+
+# fix error cor(t(x@h), vars) : 'y' must be numeric
 vars$detected<-as.numeric(vars$detected)
-pheatmap(cor(t(x@h),vars))
+vars$sum<-as.numeric(vars$sum)
+vars$mito_percent<-as.numeric(vars$subsets_Mito_percent)
+vars$subsets_Mito_percent<-NULL
+vars <- as.matrix(vars)
+
+pdf(here("plots", "snRNA-seq", "06_NMF", "nmf_qc_correlation_heatmap.pdf"))
+pheatmap(cor(t(x@h),vars), fontsize_row = 5)
+dev.off()
