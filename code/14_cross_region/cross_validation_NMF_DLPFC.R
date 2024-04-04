864e5796af6bd6ec0cbf0bc9109764ce001df3a1
Kinnary Shah <kinnaryshahh@gmail.com>
2024-04-04 13:14:17 -0400

    cross validation on DLPFC spatial data

---
+++ code/14_cross_region/cross_validation_NMF_DLPFC.R
@@ -0,0 +1,36 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+
+# Load libraries
+library(RcppML)
+library(SingleCellExperiment)
+library(here)
+library(scuttle)
+library(Matrix)
+library(singlet)
+library(spatialLIBD)
+
+# Load data
+spe <- spatialLIBD::fetch_data(type = "spe")
+
+# want to use logcounts for NMF
+spe <- logNormCounts(spe)
+
+cvnmf <- cross_validate_nmf(
+    logcounts(spe),
+    ranks=c(5,10,50,100,125,150,200),
+    n_replicates = 3,
+    tol = 1e-03,
+    maxit = 100,
+    verbose = 3,
+    L1 = 0.1,
+    L2 = 0,
+    threads = 0,
+    test_density = 0.2
+)
+
+saveRDS(cvnmf, file = here("processed-data", "14_cross_region", "DLPFC_nmf_cv_results.RDS"))
+
+pdf(here("plots", "14_cross_region", "DLPFC_nmf_cv_results.pdf"))
+plot(cvnmf)
+dev.off()
+
---
+++ code/14_cross_region/cross_validation_NMF_DLPFC.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=cv
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=30G
+#SBATCH --output=logs/cv.txt
+#SBATCH --error=logs/cv.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+#SBATCH --time=4-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript cross_validation_NMF_DLPFC.R
+
+echo "**** Job ends ****"
+date
