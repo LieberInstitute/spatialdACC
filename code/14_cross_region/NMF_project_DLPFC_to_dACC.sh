9aa003dce91e8375d308c835447b4d3c27319aad
kinnaryshah <kinnaryshahh@gmail.com>
2024-04-04 15:29:04 -0400

    project DLPFC NMF to dACC

---
+++ code/14_cross_region/NMF_project_DLPFC_to_dACC.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=nmf
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=15G
+#SBATCH --output=logs/dACC_project.txt
+#SBATCH --error=logs/dACC_project.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+#SBATCH --time=2-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript NMF_project_DLPFC_to_dACC.R
+
+echo "**** Job ends ****"
+date
+
+
