ebb1f13cfdde3a7c76aa2c2232c702162396f3a4
kinnaryshah <kinnaryshahh@gmail.com>
2024-04-09 08:38:33 -0400

    add memory

--- code/13_NMF/01_NMF_project.sh
+++ code/13_NMF/01_NMF_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=nmf
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=55G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=kinnaryshahh@gmail.com
 #SBATCH --time=2-00:00:00
