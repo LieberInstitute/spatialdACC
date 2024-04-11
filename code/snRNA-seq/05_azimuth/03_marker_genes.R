507a65e2a306a5d74a3c5e60ce8a78292e563859
Kinnary Shah <kinnaryshahh@gmail.com>
2024-04-11 10:12:15 -0400

    find top 20 marker genes for Azimuth clusters

--- code/snRNA-seq/05_azimuth/03_marker_genes.R
+++ code/snRNA-seq/05_azimuth/03_marker_genes.R
@@ -8,7 +8,28 @@ library(scran)
 load(file = here("processed-data", "snRNA-seq", "05_azimuth", "sce_azimuth.Rdata"))
 sce <- logNormCounts(sce)
 
+# replace spaces with underscores
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L2/3 IT", "L2_3_IT")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L5 ET", "L5_ET")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L5 IT", "L5_IT")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L5/6 NP", "L5_6_NP")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L6 CT", "L6_CT")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L6 IT", "L6_IT")
+colData(sce)$cellType_azimuth <- replace(colData(sce)$cellType_azimuth, colData(sce)$cellType_azimuth == "L6 IT Car3", "L6_IT_Car3")
+
 marker.info <- scoreMarkers(sce, colData(sce)$cellType_azimuth)
 marker.info
 
 save(marker.info, file = here("processed-data", "snRNA-seq", "05_azimuth", "marker_info_azimuth.Rdata"))
+
+# get top 20 markers for each cluster
+top20_markers <- lapply(marker.info, function(x) {
+  x <- x[order(x$mean.AUC, decreasing = TRUE),]
+  x <- x[1:20,]
+  return(x)
+})
+
+# write to csv file
+for (i in 1:length(top20_markers)) {
+  write.csv(top20_markers[[i]], file = here("processed-data", "snRNA-seq", "05_azimuth", paste0("top20_markers_", names(top20_markers)[i], ".csv")))
+}
