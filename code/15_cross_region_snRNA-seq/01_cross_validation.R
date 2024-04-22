cc3f7f43689fd8098d723a6f5e94b81e99198f74
Kinnary Shah <kinnaryshahh@gmail.com>
2024-04-22 14:23:01 -0400

    run cross validation for snRNA-seq NMF

---
+++ code/15_cross_region_snRNA-seq/01_cross_validation.R
@@ -0,0 +1,37 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+
+# Load libraries
+library(RcppML)
+library(SingleCellExperiment)
+library(here)
+library(scuttle)
+library(Matrix)
+library(singlet)
+library(spatialLIBD)
+
+# Load data
+sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
+sce_path <- unzip(sce_path_zip, exdir = tempdir())
+sce <- HDF5Array::loadHDF5SummarizedExperiment(
+    file.path(tempdir(), "sce_DLPFC_annotated")
+)
+
+cvnmf <- cross_validate_nmf(
+    logcounts(sce),
+    ranks=c(15,25,35,45,55,75,100),
+    n_replicates = 3,
+    tol = 1e-03,
+    maxit = 100,
+    verbose = 3,
+    L1 = 0.1,
+    L2 = 0,
+    threads = 0,
+    test_density = 0.2
+)
+
+saveRDS(cvnmf, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.RDS"))
+
+pdf(here("plots", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.pdf"))
+plot(cvnmf)
+dev.off()
+
