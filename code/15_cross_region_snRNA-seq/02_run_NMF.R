83369679a17316b6d9b60cf61e350aa53a2c50b1
Kinnary Shah <kinnaryshahh@gmail.com>
2024-05-16 12:01:16 -0400

    create heatmaps of NMF patterns

--- code/15_cross_region_snRNA-seq/02_run_NMF.R
+++ code/15_cross_region_snRNA-seq/02_run_NMF.R
@@ -8,6 +8,7 @@ library(here)
 library(scuttle)
 library(Matrix)
 library(spatialLIBD)
+library(reshape2)
 
 # Load data
 sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
@@ -34,4 +35,42 @@ x <- RcppML::nmf(assay(sce,'logcounts'),
 # Save NMF results
 saveRDS(x, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_results.RDS"))
 
+####onehot encode celltype
+data<-as.data.frame(sce$cellType_hc)
+colnames(data)<-'celltype'
+onehot_celltype <-  dcast(data = data, rownames(data) ~ celltype, length)
+rownames(onehot_celltype)<-onehot_celltype[,1]
+onehot_celltype[,1]<-as.numeric(onehot_celltype[,1])
+onehot_celltype<-onehot_celltype[order(onehot_celltype[,1],decreasing=F),]
+onehot_celltype[,1]<-NULL
+
+###correlate with nmf patterns
+pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_celltype_correlation_heatmap.pdf"))
+pheatmap(cor(t(x@h),onehot_celltype), fontsize_row = 5)
+dev.off()
+
+# aggregate NMF patterns
+
+# create dataframe
+data <- data.frame(colData(sce), t(x@h))
+
+# aggregate NMF patterns across celltypes
+# grep "NMF" to get all NMF patterns
+agg_data <- aggregate(data[,grep("nmf", colnames(data))],
+                      by=list(data$cellType_hc),
+                      FUN=mean)
+
+# move Group.1 to row names, then drop
+rownames(agg_data) <- agg_data$Group.1
+agg_data <- agg_data[,-1]
+
+pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_celltype_correlation_aggregated_heatmap.pdf"))
+p1 <- pheatmap(agg_data,
+               color=colorRampPalette(c("blue","white","red"))(100),
+               cluster_cols=T,
+               cluster_rows=T,
+               scale="column",
+               fontsize_col = 5
+)
+dev.off()
 
