081e32d4f7588cbfe2a586456cd4ecef62b0c5fb
Kinnary Shah <kinnaryshahh@gmail.com>
2024-05-16 15:47:23 -0400

    project into n=30 DLPFC data

---
+++ code/15_cross_region_snRNA-seq/05_project_into_DLPFC_new.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=nmf
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=80G
+#SBATCH --output=logs/DLPFC_30_project.txt
+#SBATCH --error=logs/DLPFC_30_project.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+#SBATCH --time=2-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 05_project_into_DLPFC_new.R
+
+echo "**** Job ends ****"
+date
+
