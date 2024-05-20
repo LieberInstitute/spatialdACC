e082f6593b245539180ed12be45ae2bb829e14f9
Kinnary Shah <kinnaryshahh@gmail.com>
2024-05-20 18:37:19 -0400

    heatmap with spatial annotations of snRNA-seq

--- code/15_cross_region_snRNA-seq/02_run_NMF.R
+++ code/15_cross_region_snRNA-seq/02_run_NMF.R
@@ -35,18 +35,18 @@ x <- RcppML::nmf(assay(sce,'logcounts'),
 # Save NMF results
 saveRDS(x, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_results.RDS"))
 
-####onehot encode celltype
-data<-as.data.frame(sce$cellType_hc)
-colnames(data)<-'celltype'
-onehot_celltype <-  dcast(data = data, rownames(data) ~ celltype, length)
-rownames(onehot_celltype)<-onehot_celltype[,1]
-onehot_celltype[,1]<-as.numeric(onehot_celltype[,1])
-onehot_celltype<-onehot_celltype[order(onehot_celltype[,1],decreasing=F),]
-onehot_celltype[,1]<-NULL
+####onehot encode layer
+data<-as.data.frame(sce$layer_annotation)
+colnames(data)<-'layer'
+onehot_layer <-  dcast(data = data, rownames(data) ~ layer, length)
+rownames(onehot_layer)<-onehot_layer[,1]
+onehot_layer[,1]<-as.numeric(onehot_layer[,1])
+onehot_layer<-onehot_layer[order(onehot_layer[,1],decreasing=F),]
+onehot_layer[,1]<-NULL
 
 ###correlate with nmf patterns
-pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_celltype_correlation_heatmap.pdf"))
-pheatmap(cor(t(x@h),onehot_celltype), fontsize_row = 5)
+pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_layer_correlation_heatmap.pdf"))
+pheatmap(cor(t(x@h),onehot_layer), fontsize_row = 5)
 dev.off()
 
 # aggregate NMF patterns
@@ -54,17 +54,17 @@ dev.off()
 # create dataframe
 data <- data.frame(colData(sce), t(x@h))
 
-# aggregate NMF patterns across celltypes
+# aggregate NMF patterns across layers
 # grep "NMF" to get all NMF patterns
 agg_data <- aggregate(data[,grep("nmf", colnames(data))],
-                      by=list(data$cellType_hc),
+                      by=list(data$layer_annotation),
                       FUN=mean)
 
 # move Group.1 to row names, then drop
 rownames(agg_data) <- agg_data$Group.1
 agg_data <- agg_data[,-1]
 
-pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_celltype_correlation_aggregated_heatmap.pdf"))
+pdf(here("plots", "15_cross_region_snRNA-seq", "nmf_DLPFC_layer_correlation_aggregated_heatmap.pdf"))
 p1 <- pheatmap(agg_data,
                color=colorRampPalette(c("blue","white","red"))(100),
                cluster_cols=T,
