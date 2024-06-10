d5ee982396415897368a9f9665b42718f69068a5
Kinnary Shah <kinnaryshahh@gmail.com>
2024-06-10 15:29:58 -0400

    spot plots for top 5 genes

--- code/15_cross_region_snRNA-seq/top_genes_factor.R
+++ code/15_cross_region_snRNA-seq/top_genes_factor.R
@@ -5,6 +5,9 @@ library(SpatialExperiment)
 library(RcppML)
 library(here)
 library(spatialLIBD)
+library(ggplot2)
+library(escheR)
+library(gridExtra)
 
 sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
 sce_path <- unzip(sce_path_zip, exdir = tempdir())
@@ -26,3 +29,78 @@ write.csv(top10, file = here("processed-data", "15_cross_region_snRNA-seq", "top
 # get top 20 genes
 top20 <- top_genes(x$w, 20)
 write.csv(top20, file = here("processed-data", "15_cross_region_snRNA-seq", "top20_genes.csv"))
+
+# visualize top 5 genes for NMF33 to see how specific they are to L5
+
+# get top 10 genes for NMF33
+gene_list <- top10["nmf33"]
+gene_list
+# 1   CLSTN2
+# 2     SNCA
+# 3     SYN2
+# 4     SYT1
+# 5    BASP1
+
+spe_DLPFC_30 <- spatialLIBD::fetch_data(type = "spatialDLPFC_Visium")
+
+# create spatial labels for DLPFC_30
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 3] <- "L2"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 8] <- "L4"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 7] <- "L6"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 5] <- "L3"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 6] <- "WM"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 4] <- "L5"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 2] <- "L1"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 1] <- "meninges"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 9] <- "WM"
+
+# remove meninges from DLPFC_30
+spe_DLPFC_30 <- spe_DLPFC_30[,spe_DLPFC_30$BayesSpace_harmony_09 != "meninges"]
+spe <- spe_DLPFC_30
+
+spe$counts_BASP1 <- counts(spe)[which(rowData(spe)$gene_name=="BASP1"),]
+
+
+spe.temp <- spe
+brains <- unique(spe.temp$subject)
+
+pdf(file = here::here("plots", "15_cross_region_snRNA-seq", "SpotPlots_NMF33", "BASP1_spot_plots.pdf"),
+    width = 21, height = 20)
+
+for (j in seq_along(brains)){
+    speb <- spe.temp[, which(spe.temp$subject == brains[j])]
+    samples <- unique(speb$sample_id)
+    print(length(samples))
+
+    if (length(samples) == 1){
+        spe_1 <- speb[, which(speb$sample_id == samples[1])]
+        p1 <- make_escheR(spe_1) |> add_fill(var="counts_BASP1", point_size = 9) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 9) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[1]))
+
+        grid.arrange(p1, nrow = 1)
+    } else if (length(samples) == 2){
+        spe_1 <- speb[, which(speb$sample_id == samples[1])]
+        p1 <- make_escheR(spe_1) |> add_fill(var="counts_BASP1", point_size = 4) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 4) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[1]))
+
+        spe_2 <- speb[, which(speb$sample_id == samples[2])]
+        p2 <- make_escheR(spe_2) |> add_fill(var="counts_BASP1", point_size = 4) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 4) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[2]))
+        grid.arrange(p1, p2, nrow = 2)
+    } else if (length(samples) == 3){
+        spe_1 <- speb[, which(speb$sample_id == samples[1])]
+        p1 <- make_escheR(spe_1) |> add_fill(var="counts_BASP1", point_size = 4) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 4) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[1]))
+
+        spe_2 <- speb[, which(speb$sample_id == samples[2])]
+        p2 <- make_escheR(spe_2) |> add_fill(var="counts_BASP1", point_size = 4) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 4) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[2]))
+
+        spe_3 <- speb[, which(speb$sample_id == samples[3])]
+        p3 <- make_escheR(spe_3) |> add_fill(var="counts_BASP1", point_size = 4) |> add_ground(var="BayesSpace_harmony_09", stroke=0.5, point_size = 4) +
+            scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", samples[3]))
+        grid.arrange(p1, p2, p3, nrow = 2)
+    }
+}
+
+dev.off()
