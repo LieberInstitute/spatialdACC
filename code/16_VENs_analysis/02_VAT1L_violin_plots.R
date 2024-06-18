a8ada9af5c1213b147e3e958521cc9a2234f1d3e
Kinnary Shah <kinnaryshahh@gmail.com>
2024-06-18 14:06:32 -0400

    violin plots of VAT1L and DRD5 for DLPFC

--- code/16_VENs_analysis/02_VAT1L_violin_plots.R
+++ code/16_VENs_analysis/02_VAT1L_violin_plots.R
@@ -7,6 +7,7 @@ library(spatialLIBD)
 library(here)
 library(pheatmap)
 library(scater)
+library(scran)
 
 # create violin plots to show the distribution of VAT1L expression in each cluster
 load(file = here("processed-data", "snRNA-seq", "05_azimuth", "sce_azimuth.Rdata"))
@@ -24,11 +25,11 @@ spe$counts_VAT1L <- counts(spe)[which(rowData(spe)$gene_name=="VAT1L"),]
 spe$logcounts_VAT1L <- logcounts(spe)[which(rowData(spe)$gene_name=="VAT1L"),]
 
 spe$PRECAST_cluster <- unfactor(spe$PRECAST_cluster)
-spe$PRECAST_cluster[spe$PRECAST_cluster == 3] <- "WM1"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 8] <- "WM2"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 7] <- "WM-CC"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 5] <- "L6b"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 6] <- "L6a"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 3] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 8] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 7] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 5] <- "L6"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 6] <- "L6"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 4] <- "L5"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 2] <- "L3"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 1] <- "L2"
@@ -52,5 +53,67 @@ plotColData(sce, x = "cellType_azimuth", y = "logcounts_VAT1L") +
 plotColData(spe, x = "PRECAST_cluster", y = "logcounts_VAT1L") +
     ggtitle("VAT1L Logcounts by PRECAST Cluster") +
     facet_wrap(~ spe$PRECAST_cluster, scales = "free_x", nrow = 1)
+
+dev.off()
+
+# create violin plots to show the distribution of VAT1L expression in each cluster
+sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
+sce_path <- unzip(sce_path_zip, exdir = tempdir())
+sce <- HDF5Array::loadHDF5SummarizedExperiment(
+    file.path(tempdir(), "sce_DLPFC_annotated")
+)
+
+assay(sce, "logcounts") <- as(assay(sce, "logcounts"), "dgCMatrix")
+
+sce$counts_VAT1L <- counts(sce)[which(rowData(sce)$gene_name=="VAT1L"),]
+sce$logcounts_VAT1L <- logcounts(sce)[which(rowData(sce)$gene_name=="VAT1L"),]
+
+# create violin plots to show the distribution of VAT1L expression in each spatial domain
+spe_DLPFC_30 <- spatialLIBD::fetch_data(type = "spatialDLPFC_Visium")
+
+spe_DLPFC_30$counts_VAT1L <- counts(spe_DLPFC_30)[which(rowData(spe_DLPFC_30)$gene_name=="VAT1L"),]
+spe_DLPFC_30$logcounts_VAT1L <- logcounts(spe_DLPFC_30)[which(rowData(spe_DLPFC_30)$gene_name=="VAT1L"),]
+
+# create spatial labels for DLPFC_30
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 3] <- "L2"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 8] <- "L4"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 7] <- "L6"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 5] <- "L3"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 6] <- "WM"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 4] <- "L5"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 2] <- "L1"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 1] <- "meninges"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 9] <- "WM"
+
+spe_DLPFC_30$cluster <- spe_DLPFC_30$BayesSpace_harmony_09
+
+sce_noNA <- sce[,!is.na(sce$layer_annotation)]
+
+pdf(file = here::here("plots", "16_VENs_analysis", "VAT1L_violin_plots_DLPFC_30.pdf"),
+    width = 21, height = 20)
+
+plotColData(sce_noNA, x = "layer_annotation", y = "counts_VAT1L") +
+    ggtitle("Single Nucleus VAT1L Counts by Layer Annotation") +
+    facet_wrap(~ sce_noNA$layer_annotation, scales = "free_x", nrow = 1)
+
+plotColData(sce, x = "cellType_broad_hc", y = "counts_VAT1L") +
+    ggtitle("Single Nucleus VAT1L Counts by Broad HC") +
+    facet_wrap(~ sce$cellType_broad_hc, scales = "free_x", nrow = 1)
+
+plotColData(spe_DLPFC_30, x = "cluster", y = "counts_VAT1L") +
+    ggtitle("Visium VAT1L Counts by Cluster") +
+    facet_wrap(~ spe_DLPFC_30$cluster, scales = "free_x", nrow = 1)
+
+plotColData(sce_noNA, x = "layer_annotation", y = "logcounts_VAT1L") +
+    ggtitle("Single Nucleus VAT1L Logcounts by Layer Annotation") +
+    facet_wrap(~ sce_noNA$layer_annotation, scales = "free_x", nrow = 1)
+
+plotColData(sce, x = "cellType_broad_hc", y = "logcounts_VAT1L") +
+    ggtitle("Single Nucleus VAT1L Logcounts by Broad HC") +
+    facet_wrap(~ sce$cellType_broad_hc, scales = "free_x", nrow = 1)
+
+plotColData(spe_DLPFC_30, x = "cluster", y = "logcounts_VAT1L") +
+    ggtitle("Visium VAT1L Logcounts by Cluster") +
+    facet_wrap(~ spe_DLPFC_30$cluster, scales = "free_x", nrow = 1)
 
 dev.off()
--- code/16_VENs_analysis/03_DRD5_violin_plots.R
+++ code/16_VENs_analysis/03_DRD5_violin_plots.R
@@ -23,11 +23,11 @@ spe$counts_DRD5 <- counts(spe)[which(rowData(spe)$gene_name=="DRD5"),]
 spe$logcounts_DRD5 <- logcounts(spe)[which(rowData(spe)$gene_name=="DRD5"),]
 
 spe$PRECAST_cluster <- unfactor(spe$PRECAST_cluster)
-spe$PRECAST_cluster[spe$PRECAST_cluster == 3] <- "WM1"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 8] <- "WM2"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 7] <- "WM-CC"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 5] <- "L6b"
-spe$PRECAST_cluster[spe$PRECAST_cluster == 6] <- "L6a"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 3] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 8] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 7] <- "WM"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 5] <- "L6"
+spe$PRECAST_cluster[spe$PRECAST_cluster == 6] <- "L6"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 4] <- "L5"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 2] <- "L3"
 spe$PRECAST_cluster[spe$PRECAST_cluster == 1] <- "L2"
@@ -53,3 +53,66 @@ plotColData(spe, x = "PRECAST_cluster", y = "logcounts_DRD5") +
     facet_wrap(~ spe$PRECAST_cluster, scales = "free_x", nrow = 1)
 
 dev.off()
+
+# create violin plots to show the distribution of DRD5 expression in each cluster
+sce_path_zip <- fetch_data("spatialDLPFC_snRNAseq")
+sce_path <- unzip(sce_path_zip, exdir = tempdir())
+sce <- HDF5Array::loadHDF5SummarizedExperiment(
+    file.path(tempdir(), "sce_DLPFC_annotated")
+)
+
+assay(sce, "logcounts") <- as(assay(sce, "logcounts"), "dgCMatrix")
+
+sce$counts_DRD5 <- counts(sce)[which(rowData(sce)$gene_name=="DRD5"),]
+sce$logcounts_DRD5 <- logcounts(sce)[which(rowData(sce)$gene_name=="DRD5"),]
+
+# create violin plots to show the distribution of DRD5 expression in each spatial domain
+spe_DLPFC_30 <- spatialLIBD::fetch_data(type = "spatialDLPFC_Visium")
+
+spe_DLPFC_30$counts_DRD5 <- counts(spe_DLPFC_30)[which(rowData(spe_DLPFC_30)$gene_name=="DRD5"),]
+spe_DLPFC_30$logcounts_DRD5 <- logcounts(spe_DLPFC_30)[which(rowData(spe_DLPFC_30)$gene_name=="DRD5"),]
+
+# create spatial labels for DLPFC_30
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 3] <- "L2"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 8] <- "L4"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 7] <- "L6"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 5] <- "L3"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 6] <- "WM"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 4] <- "L5"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 2] <- "L1"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 1] <- "meninges"
+spe_DLPFC_30$BayesSpace_harmony_09[spe_DLPFC_30$BayesSpace_harmony_09 == 9] <- "WM"
+
+spe_DLPFC_30$cluster <- spe_DLPFC_30$BayesSpace_harmony_09
+
+sce_noNA <- sce[,!is.na(sce$layer_annotation)]
+
+pdf(file = here::here("plots", "16_VENs_analysis", "DRD5_violin_plots_DLPFC_30.pdf"),
+    width = 21, height = 20)
+
+plotColData(sce_noNA, x = "layer_annotation", y = "counts_DRD5") +
+    ggtitle("Single Nucleus DRD5 Counts by Layer Annotation") +
+    facet_wrap(~ sce_noNA$layer_annotation, scales = "free_x", nrow = 1)
+
+plotColData(sce, x = "cellType_broad_hc", y = "counts_DRD5") +
+    ggtitle("Single Nucleus DRD5 Counts by Broad HC") +
+    facet_wrap(~ sce$cellType_broad_hc, scales = "free_x", nrow = 1)
+
+plotColData(spe_DLPFC_30, x = "cluster", y = "counts_DRD5") +
+    ggtitle("Visium DRD5 Counts by Cluster") +
+    facet_wrap(~ spe_DLPFC_30$cluster, scales = "free_x", nrow = 1)
+
+plotColData(sce_noNA, x = "layer_annotation", y = "logcounts_DRD5") +
+    ggtitle("Single Nucleus DRD5 Logcounts by Layer Annotation") +
+    facet_wrap(~ sce_noNA$layer_annotation, scales = "free_x", nrow = 1)
+
+plotColData(sce, x = "cellType_broad_hc", y = "logcounts_DRD5") +
+    ggtitle("Single Nucleus DRD5 Logcounts by Broad HC") +
+    facet_wrap(~ sce$cellType_broad_hc, scales = "free_x", nrow = 1)
+
+plotColData(spe_DLPFC_30, x = "cluster", y = "logcounts_DRD5") +
+    ggtitle("Visium DRD5 Logcounts by Cluster") +
+    facet_wrap(~ spe_DLPFC_30$cluster, scales = "free_x", nrow = 1)
+
+dev.off()
+
