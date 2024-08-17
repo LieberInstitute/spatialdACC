9f8db6be9691f31bf3cba1cde36890e3768e2973
Kinnary Shah <kinnaryshahh@gmail.com>
2024-08-17 08:45:14 -0400

    pseudobulking loop for counts mats

--- code/18_PsychENCODE_NMF/01_pseudobulking.R
+++ code/18_PsychENCODE_NMF/01_pseudobulking.R
@@ -1,114 +1,73 @@
 library(SingleCellExperiment)
-library(zellkonverter)
 library(spatialLIBD)
 library(here)
+library(zellkonverter)
 
-# need R/4.4.x to use zell konverter
-
-# read in meta data
-# Read the tab-delimited file into R
+# Read in meta data
 url <- "https://brainscope.gersteinlab.org/data/sample_metadata/PEC2_sample_metadata.txt"
 data <- read.delim(url, header = TRUE)
 
-# paths to h5ad files
+# Paths to h5ad files
 file_path <- "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/raw-data/psychENCODE/version5/"
 
+# List of dataset files
 data_list <- c("CMC_annotated.h5ad", "DevBrain-snRNAseq_annotated.h5ad",
                "IsoHuB_annotated.h5ad",
                "MultiomeBrain-DLPFC_annotated.h5ad", "PTSDBrainomics_annotated.h5ad",
-               "SZBDMulti-Seq_annotated.h5ad", "UCLA-ASD_annotated_mismatches_removed.h5ad")
+               "SZBDMulti-Seq_annotated.h5ad",
+               "UCLA-ASD_annotated_mismatches_removed.h5ad")
 
-# CMC
-dataset <- "CMC"
-file_name = "CMC_annotated.h5ad"
-file <- paste0(file_path, file_name)
-sce <- readH5AD(file)
+# Process each dataset
+for (dataset_file in data_list) {
 
-colData(sce)$cellType <- as.factor(make.names(colData(sce)$subclass))
-table(sce$cellType)
+    # Extract dataset name
+    dataset <- gsub("_annotated.h5ad|_annotated_mismatches_removed.h5ad", "", dataset_file)
 
-rowData(sce)$gene_name <- rownames(sce) # save gene name as column of rowData
-rownames(sce) <- rowData(sce)$featureid # have to make row names of object the ensembl id instead of gene names
+    # Construct file path for the h5ad file
+    file <- paste0(file_path, dataset_file)
 
-## Logcounts
-# default “X” contain the log-normalized counts
-message(Sys.time(), " revert to counts")
+    # Read the H5AD file
+    sce <- readH5AD(file)
 
-## check for all 0s (just first 100 cols for mem)
-stopifnot(any(assays(sce)$X[, 1:100] != 0))
+    # Update colData and rowData
+    colData(sce)$cellType <- as.factor(make.names(colData(sce)$subclass))
+    table(sce$cellType)
 
-counts(sce) <- assays(sce)$X # change to normalized counts
-counts(sce)@x <- 2^(counts(sce)@x) - 1 ## remove log2(counts + 1)
+    rowData(sce)$gene_name <- rownames(sce)  # Save gene name as column of rowData
+    rownames(sce) <- rowData(sce)$featureid  # Use featureid as row names
 
-# filter out Cohort = CMC and Disorder = control from metadata
-# only get Individual_ID for CMC and Disorder = control
-data_sub <- data[data$Cohort == "CMC" & data$Disorder == "control", c("Individual_ID", "Cohort", "Disorder")]
+    # Load raw counts matrix
+    raw_csv_path <- here("processed-data", "18_PsychENCODE_NMF", paste0("raw_", dataset, ".csv"))
+    counts <- read.csv(raw_csv_path)
 
-# only keep cells that have colData(sce)$individualID in data_sub$Individual_ID
-sce <- sce[, colData(sce)$individualID %in% data_sub$Individual_ID]
+    counts <- t(counts)
+    rownames(counts) <- rownames(assay(sce, "X"))
+    colnames(counts) <- colnames(assay(sce, "X"))
 
-#### Pseudobulk ####
-message(Sys.time(), " Pseudobulk")
-sce_pseudo <- registration_pseudobulk(sce,
-                                      var_registration = "cellType",
-                                      var_sample_id = "individualID",
-                                      covars = NULL
-)
+    assays(sce)$counts <- counts
 
-message("\nSCE Pseudobulk Dimensions:")
-dim(sce_pseudo)
+    # Subset data
+    data_sub <- data[data$Cohort == dataset & data$Disorder == "control", c("Individual_ID", "Cohort", "Disorder")]
 
-## Save results
-saveRDS(sce_pseudo,
-        file = here(
-            "processed-data", "18_PsychENCODE_NMF", "pseudobulk",
-            paste0("pseudobulk_", dataset, ".rds")
-        )
-)
+    # Filter SCE based on Individual_ID
+    sce <- sce[, colData(sce)$individualID %in% data_sub$Individual_ID]
 
-# UCLA-ASD
-dataset <- "UCLA-ASD"
-file_name = "UCLA-ASD_annotated_mismatches_removed.h5ad"
-file <- paste0(file_path, file_name)
-sce <- readH5AD(file)
+    # Pseudobulk
+    message(Sys.time(), " Pseudobulk for dataset: ", dataset)
+    sce_pseudo <- registration_pseudobulk(sce,
+                                          var_registration = "cellType",
+                                          var_sample_id = "individualID",
+                                          covars = NULL
+    )
 
-colData(sce)$cellType <- as.factor(make.names(colData(sce)$subclass))
-table(sce$cellType)
+    message("\nSCE Pseudobulk Dimensions for dataset ", dataset, ":")
+    dim(sce_pseudo)
 
-rowData(sce)$gene_name <- rownames(sce) # save gene name as column of rowData
-rownames(sce) <- rowData(sce)$featureid # have to make row names of object the ensembl id instead of gene names
-
-## Logcounts
-# default “X” contain the log-normalized counts
-message(Sys.time(), " revert to counts")
-
-## check for all 0s (just first 100 cols for mem)
-stopifnot(any(assays(sce)$X[, 1:100] != 0))
-
-counts(sce) <- assays(sce)$X # change to normalized counts
-counts(sce)@x <- 2^(counts(sce)@x) - 1 ## remove log2(counts + 1)
-
-data_sub <- data[data$Cohort == "UCLA-ASD" & data$Disorder == "control", c("Individual_ID", "Cohort", "Disorder")]
-
-# only keep cells that have colData(sce)$individualID in data_sub$Individual_ID
-sce <- sce[, colData(sce)$individualID %in% data_sub$Individual_ID]
-
-#### Pseudobulk ####
-message(Sys.time(), " Pseudobulk")
-sce_pseudo <- registration_pseudobulk(sce,
-                                      var_registration = "cellType",
-                                      var_sample_id = "individualID",
-                                      covars = NULL
-)
-
-message("\nSCE Pseudobulk Dimensions:")
-dim(sce_pseudo)
-
-## Save results
-saveRDS(sce_pseudo,
-        file = here(
-            "processed-data", "18_PsychENCODE_NMF", "pseudobulk",
-            paste0("pseudobulk_", dataset, ".rds")
-        )
-)
-
+    # Save results
+    saveRDS(sce_pseudo,
+            file = here(
+                "processed-data", "18_PsychENCODE_NMF", "pseudobulk",
+                paste0("pseudobulk_", dataset, ".rds")
+            )
+    )
+}
---
+++ code/18_PsychENCODE_NMF/01_pseudobulking.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=pseudo
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=170G
+#SBATCH --output=logs/pseudo.txt
+#SBATCH --error=logs/pseudo.txt
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+#SBATCH --time=2-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+python 01_pseudobulking.R
+
+echo "**** Job ends ****"
+date
