fb95a05632ad5900980eebe7ddd0b3b5c36e4c7b
Kinnary Shah <kinnaryshahh@gmail.com>
2024-08-19 16:03:16 -0400

    combine counts mats and then normalize

--- code/18_PsychENCODE_NMF/02_preprocessing.R
+++ code/18_PsychENCODE_NMF/02_preprocessing.R
@@ -17,6 +17,15 @@ sce_pseudo5 <- readRDS(pseudo_files[5])
 sce_pseudo6 <- readRDS(pseudo_files[6])
 sce_pseudo7 <- readRDS(pseudo_files[7])
 
+# get the counts matrices
+counts1 <- counts(sce_pseudo1)
+counts2 <- counts(sce_pseudo2)
+counts3 <- counts(sce_pseudo3)
+counts4 <- counts(sce_pseudo4)
+counts5 <- counts(sce_pseudo5)
+counts6 <- counts(sce_pseudo6)
+counts7 <- counts(sce_pseudo7)
+
 # combine the logcounts matrices
 logcounts1 <- logcounts(sce_pseudo1)
 logcounts2 <- logcounts(sce_pseudo2)
@@ -25,6 +34,50 @@ logcounts4 <- logcounts(sce_pseudo4)
 logcounts5 <- logcounts(sce_pseudo5)
 logcounts6 <- logcounts(sce_pseudo6)
 logcounts7 <- logcounts(sce_pseudo7)
+
+# List of logcounts variables
+logcounts_list <- list(logcounts1, logcounts2, logcounts3, logcounts4, logcounts5, logcounts6, logcounts7)
+
+# Iterate over the logcounts list
+for (i in seq_along(logcounts_list)) {
+    # Get the current logcounts matrix
+    logcounts_matrix <- logcounts_list[[i]]
+
+    # Count the number of negative values
+    num_negative_values <- sum(logcounts_matrix < 0)
+
+    # Calculate the proportion of negative values
+    total_values <- length(logcounts_matrix)
+    proportion_negative <- num_negative_values / total_values
+
+    # Print the results
+    cat("Matrix", i, "\n")
+    cat("Number of negative values:", num_negative_values, "\n")
+    cat("Proportion of negative values:", proportion_negative, "\n\n")
+}
+
+
+# do the same for counts matrices
+# List of counts variables
+counts_list <- list(counts1, counts2, counts3, counts4, counts5, counts6, counts7)
+
+# Iterate over the counts list
+for (i in seq_along(counts_list)) {
+    # Get the current counts matrix
+    counts_matrix <- counts_list[[i]]
+
+    # Count the number of negative values
+    num_negative_values <- sum(counts_matrix == 0)
+
+    # Calculate the proportion of negative values
+    total_values <- length(counts_matrix)
+    proportion_negative <- num_negative_values / total_values
+
+    # Print the results
+    cat("Matrix", i, "\n")
+    cat("Number of zero values:", num_negative_values, "\n")
+    cat("Proportion of zero values:", proportion_negative, "\n\n")
+}
 
 # problem that rownames are not the same
 # just get overlaps between all datasets
@@ -38,6 +91,15 @@ rownames7 <- rownames(logcounts7)
 
 # get overlaps
 overlaps <- Reduce(intersect, list(rownames1, rownames2, rownames3, rownames4, rownames5, rownames6, rownames7))
+
+# filter out non-overlapping genes in counts
+counts1 <- counts1[overlaps, ]
+counts2 <- counts2[overlaps, ]
+counts3 <- counts3[overlaps, ]
+counts4 <- counts4[overlaps, ]
+counts5 <- counts5[overlaps, ]
+counts6 <- counts6[overlaps, ]
+counts7 <- counts7[overlaps, ]
 
 # filter out non-overlapping genes
 logcounts1 <- logcounts1[overlaps, ]
@@ -56,11 +118,17 @@ all(match(rownames(logcounts1), rownames(logcounts5)) == 1:nrow(logcounts1))
 all(match(rownames(logcounts1), rownames(logcounts6)) == 1:nrow(logcounts1))
 all(match(rownames(logcounts1), rownames(logcounts7)) == 1:nrow(logcounts1))
 
+#combine the counts matrices
+counts_combined <- cbind(counts1, counts2, counts3, counts4, counts5, counts6, counts7)
+
 # combine the logcounts matrices
 logcounts_combined <- cbind(logcounts1, logcounts2, logcounts3, logcounts4, logcounts5, logcounts6, logcounts7)
 
 # create a SingleCellExperiment object
-sce <- SingleCellExperiment(assays = list(logcounts = logcounts_combined))
+sce <- SingleCellExperiment(assays = list(counts = counts_combined))
+
+# compute the logcounts
+sce <- logNormCounts(sce)
 
 # save
-saveRDS(sce, file = here("processed-data", "18_PsychENCODE_NMF", "pseudobulk", "pseudobulk_combined.rds"))
+saveRDS(sce, file = here("processed-data", "18_PsychENCODE_NMF", "pseudobulk_combined.rds"))
