b39fad22f982ce4a9b0f1a3eae381ea5cf045c89
Kinnary Shah <kinnaryshahh@gmail.com>
2024-08-20 16:03:25 -0400

    vis pseudobulked samples

--- code/18_PsychENCODE_NMF/02_preprocessing.R
+++ code/18_PsychENCODE_NMF/02_preprocessing.R
@@ -5,6 +5,9 @@
 library(SingleCellExperiment)
 library(spatialLIBD)
 library(here)
+library(scater)
+library(RColorBrewer)
+library(viridis)
 
 pseudo_path <- here("processed-data", "18_PsychENCODE_NMF", "pseudobulk")
 # find all files in this path
@@ -130,5 +133,157 @@ sce <- SingleCellExperiment(assays = list(counts = counts_combined))
 # compute the logcounts
 sce <- logNormCounts(sce)
 
+# add in col level info to the sce object for plotting
+# subject
+# cell type
+rownames_data <- rownames(colData(sce))
+split_data <- do.call(rbind, strsplit(rownames_data, "_(?!.*_)", perl = TRUE))
+
+colData(sce)$Subject <- split_data[, 1]
+colData(sce)$Cell_Type <- split_data[, 2]
+
+# from subject - infer study as well
+
+# Read in meta data
+url <- "https://brainscope.gersteinlab.org/data/sample_metadata/PEC2_sample_metadata.txt"
+data <- read.delim(url, header = TRUE)
+
+# make a dictionary of the Cohort to the Individual_ID in the metadata
+cohort_dict <- setNames(data$Cohort, data$Individual_ID)
+
+colData(sce)$Study <- cohort_dict[colData(sce)$Subject]
+
 # save
 saveRDS(sce, file = here("processed-data", "18_PsychENCODE_NMF", "pseudobulk_combined.rds"))
+
+pca <- prcomp(t(assays(sce)$logcounts))
+metadata(sce) <- list("PCA_var_explained" = jaffelab::getPcaVars(pca)[seq_len(20)])
+pca_pseudo <- pca$x[, seq_len(20)]
+colnames(pca_pseudo) <- paste0("PC", sprintf("%02d", seq_len(ncol(pca_pseudo))))
+reducedDims(sce) <- list(PCA = pca_pseudo)
+
+palette <- c(brewer.pal(9, "Set1"), brewer.pal(8, "Set2"), viridis(5), rainbow(5))
+palette1 <- palette[1:27]
+
+palette2 <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+             "#FF7F00", "#FFFF33", "#A65628")
+
+palette3 <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+             "#FF7F00", "#FFFF33", "#A65628", "#F781BF",
+             "#999999", "#66C2A5", "#FC8D62", "#8DA0CB",
+             "#E78AC3", "#A6D854", "#FFD92F", "#E5C494",
+             "#B3B3B3", "#8B4513", "#1E90FF")
+
+
+# create new variable called Neuron -- values are TRUE or FALSE
+# Astro - F
+# Chandelier - T
+# Endo - F
+# Immune - F
+# L2.3.IT - T
+# L4.IT - T
+# L5.6.NP - T
+# L5.ET - T
+# L5.IT - T
+# L6.CT - T
+# L6.IT - T
+# L6.IT.Car3 - T
+# L6b - T
+# Lamp5 - T
+# Lamp5.Lhx6 - T
+# Micro - F
+# Oligo - F
+# OPC - F
+# Pax6 - T
+# PC - F (Pericyte)
+# Pvalb - T
+# SMC - F
+# Sncg - T
+# Sst - T
+# Sst.Chodl - T
+# Vip - T
+# VLMC - F
+
+colData(sce)$Neuron <- colData(sce)$Cell_Type %in% c("Chandelier", "L2.3.IT", "L4.IT", "L5.6.NP", "L5.ET", "L5.IT", "L6.CT", "L6.IT", "L6.IT.Car3", "L6b", "Lamp5", "Lamp5.Lhx6", "Pax6", "Pvalb", "Sncg", "Sst", "Sst.Chodl", "Vip")
+
+# create a new variable called NeuronTypes
+# the values are the same as the Cell_Type variable
+# but the values that are not neurons are set to "Non-Neuron"
+colData(sce)$NeuronTypes <- colData(sce)$Cell_Type
+colData(sce)$NeuronTypes[!colData(sce)$Neuron] <- "Non-Neuron"
+
+## Plot PCs
+pdf(file = here("plots", "18_PsychENCODE_NMF", "pseudobulk_PC.pdf"), width = 14, height = 14)
+
+plotPCA(
+    sce,
+    colour_by = "Neuron",
+    ncomponents = 2,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained)
+
+plotPCA(
+    sce,
+    colour_by = "Cell_Type",
+    ncomponents = 2,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+    scale_colour_manual(values = palette1)
+
+plotPCA(
+    sce,
+    colour_by = "NeuronTypes",
+    ncomponents = 2,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+    scale_colour_manual(values = palette3)
+
+
+plotPCA(
+    sce,
+    colour_by = "Study",
+    ncomponents = 2,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+        scale_colour_manual(values = palette2)
+
+plotPCA(
+    sce,
+    colour_by = "Neuron",
+    ncomponents = 3,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained)
+
+plotPCA(
+    sce,
+    colour_by = "Cell_Type",
+    ncomponents = 3,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+    scale_colour_manual(values = palette1)
+
+plotPCA(
+    sce,
+    colour_by = "NeuronTypes",
+    ncomponents = 3,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+    scale_colour_manual(values = palette3)
+
+
+plotPCA(
+    sce,
+    colour_by = "Study",
+    ncomponents = 3,
+    point_size = 1,
+    percentVar = metadata(sce)$PCA_var_explained) +
+    scale_colour_manual(values = palette2)
+
+vars <- getVarianceExplained(sce,
+                             variables = c("Study","Cell_Type")
+)
+
+
+plotExplanatoryVariables(vars)
+
+dev.off()
