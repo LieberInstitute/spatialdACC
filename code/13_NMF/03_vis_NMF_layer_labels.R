55fb004c02a42d8952a62f724d415bc273814e4a
Kinnary Shah <kinnaryshahh@gmail.com>
2024-09-24 15:29:49 -0400

    add more NMF factors

--- code/13_NMF/03_vis_NMF_layer_labels.R
+++ code/13_NMF/03_vis_NMF_layer_labels.R
@@ -33,25 +33,6 @@ make_plots <- function(factor, dACC_id){
     p1 <- make_escheR(spe_1) |>
         add_fill(var=factor, point_size = 3) |>
         add_ground(var=dACC_layer, stroke=0.5, point_size = 3) +
-        scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", dACC_id)) +
-        labs(color = "layer") +
-        theme(
-            legend.text = element_text(size = 5), # Smaller text size
-            legend.title = element_text(size = 8), # Smaller title size
-            legend.key.size = unit(0.5, "lines"), # Smaller key size
-            legend.spacing.x = unit(0.3, 'cm'), # Adjust spacing between keys
-            legend.spacing.y = unit(0.3, 'cm'))  # Adjust spacing between rows
-
-
-    return(list(p1))
-}
-
-make_plots <- function(factor, dACC_id){
-
-    spe_1 <- spe[, which(spe$sample_id == dACC_id)]
-    p1 <- make_escheR(spe_1) |>
-        add_fill(var=factor, point_size = 3) |>
-        add_ground(var=dACC_layer, stroke=0.5, point_size = 3) +
         scale_color_manual(values = c(
             "L2" = "#E41A1C",   # Bright red
             "L3" = "#377EB8",   # Strong blue
@@ -62,7 +43,7 @@ make_plots <- function(factor, dACC_id){
             "L1" = "#00CED1"    # Dark turquoise
         )) +
         scale_fill_gradient(low = "white", high = "black") + labs(title = paste0("Sample ", dACC_id)) +
-        labs(color = "layer") +
+        labs(color = "layer", caption = "single nucleus dACC NMF") +
         theme(
             legend.text = element_text(size = 5), # Smaller text size
             legend.title = element_text(size = 8), # Smaller title size
@@ -85,8 +66,17 @@ plots_3 <- make_plots("nmf3", "V12Y31-080_B1")
 #L3
 plots_11 <- make_plots("nmf11", "V12N28-332_D1")
 
-#L5
+#L5a
 plots_38 <- make_plots("nmf38", "V12Y31-080_C1")
+
+#L5b
+plots_61 <- make_plots("nmf61", "V12N28-332_D1")
+
+#L6a
+plots_15 <- make_plots("nmf15", "V12N28-332_A1")
+
+#kind of L6b and a
+plots_35 <- make_plots("nmf35", "V12N28-331_A1")
 
 #WM
 plots_6 <- make_plots("nmf6", "V12J03-002_A1")
@@ -102,12 +92,13 @@ plots_59 <- make_plots("nmf59", "V12N28-334_B1")
 
 pdf(here::here("plots", "13_NMF", "compare_NMF.pdf"), width = 10, height = 10)
 
-grid.arrange(grobs = plots_21, nrow = 1)
-grid.arrange(grobs = plots_64, nrow = 1)
 grid.arrange(grobs = plots_65, nrow = 1)
 grid.arrange(grobs = plots_3, nrow = 1)
 grid.arrange(grobs = plots_11, nrow = 1)
 grid.arrange(grobs = plots_38, nrow = 1)
+grid.arrange(grobs = plots_61, nrow = 1)
+grid.arrange(grobs = plots_15, nrow = 1)
+grid.arrange(grobs = plots_35, nrow = 1)
 grid.arrange(grobs = plots_6, nrow = 1)
 grid.arrange(grobs = plots_13, nrow = 1)
 grid.arrange(grobs = plots_27, nrow = 1)
