f612221ed6443d4e8a4ad6c62da1ffb913eec07d
Kinnary Shah <kinnaryshahh@gmail.com>
2024-09-25 14:09:07 -0400

    spatial registration with n=30 DLPFC

---
+++ code/12_spatial_registration/DLPFC_30_spatial_registration.R
@@ -0,0 +1,73 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+
+library("here")
+library("spatialLIBD")
+library("SingleCellExperiment")
+library("stringr")
+
+# get reference layer enrichment statistics
+#k <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+
+k=9
+nnSVG_precast_name <- paste0("nnSVG_PRECAST_captureArea_", k)
+load(file = here("processed-data", "11_differential_expression", "pseudobulk",
+                 "nnSVG_precast_DE", paste0(nnSVG_precast_name,".Rdata")))
+
+# load DLPFC manual annotations
+spe <- spatialLIBD::fetch_data(type = "spatialDLPFC_Visium")
+# create spatial labels for DLPFC_30
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 3] <- "L2"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 8] <- "L4"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 7] <- "L6"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 5] <- "L3"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 6] <- "WM"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 4] <- "L5"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 2] <- "L1"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 1] <- "meninges"
+spe$BayesSpace_harmony_09[spe$BayesSpace_harmony_09 == 9] <- "WM"
+# remove meninges
+spe <- spe[ , which(spe$BayesSpace_harmony_09 != "meninges")]
+
+table(spe$sample_id, spe$BayesSpace_harmony_09)
+
+spe_modeling_results <- registration_wrapper(
+    spe,
+    var_registration = "BayesSpace_harmony_09",
+    var_sample_id = "sample_id",
+    gene_ensembl = "gene_id",
+    gene_name = "gene_name"
+)
+
+## extract t-statics and rename
+registration_t_stats <- spe_modeling_results$enrichment[, grep("^t_stat", colnames(spe_modeling_results$enrichment))]
+colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_stats))
+
+## cell types x gene
+dim(registration_t_stats)
+
+## check out table
+registration_t_stats[1:5, 1:5]
+
+cor_layer <- layer_stat_cor(
+    stats = registration_t_stats,
+    modeling_results = modeling_results,
+    model_type = "enrichment",
+    top_n = 100
+)
+
+cor_layer
+save(cor_layer, file = here("processed-data", "12_spatial_registration",paste0("DLPFC_30_",nnSVG_precast_name,".rds")))
+
+pdf(file = here::here("plots", "12_spatial_registration", "DLPFC_manual",
+                      paste0("DLPFC_30_",nnSVG_precast_name,"_heatmap.pdf")), width = 14, height = 14)
+layer_stat_cor_plot(cor_layer, max = max(cor_layer))
+title("DLPFC n=30 vs. dACC")
+dev.off()
+
+anno <- annotate_registered_clusters(
+    cor_stats_layer = cor_layer,
+    confidence_threshold = 0.25,
+    cutoff_merge_ratio = 0.25
+)
+
+anno
