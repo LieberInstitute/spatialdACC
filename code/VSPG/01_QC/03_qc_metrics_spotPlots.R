6f67123b846939e57a80c47217cfbae3761bec0d
Kinnary Shah <kinnaryshahh@gmail.com>
2024-10-01 13:29:58 -0400

    vis MOBP in SPG data

--- code/VSPG/01_QC/03_qc_metrics_spotPlots.R
+++ code/VSPG/01_QC/03_qc_metrics_spotPlots.R
@@ -30,3 +30,40 @@ for (i in seq_along(brains)){
 }
 
 dev.off()
+
+# plot spatial expression of top-ranked SVG
+ix <- which(rowData(spe)$gene_name == "MOBP")
+ix_name <- rowData(spe)$gene_name[ix]
+
+plot_list <- list()
+# use this code but make a separate plot for each brain
+for (i in seq_along(brains)){
+
+    speb <- spe[, which(spe$brnum == brains[i])]
+    samples <- unique(speb$sample_id)
+    print(length(samples))
+
+    df <- as.data.frame(cbind(spatialCoords(speb), expr = counts(speb)[ix, ]))
+
+    p <- ggplot(df, aes(x = pxl_col_in_fullres, y = pxl_row_in_fullres,
+                        color = expr)) +
+        geom_point(size = 0.8) +
+        coord_fixed() +
+        scale_y_reverse() +
+        scale_color_gradient(low = "gray90", high = "blue",
+                             trans = "sqrt", breaks = range(df$expr),
+                             name = "counts") +
+        ggtitle(samples) +
+        theme_bw() +
+        theme(plot.title = element_text(face = "italic"),
+              panel.grid = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank())
+
+    plot_list[[i]] <- p
+}
+
+pdf(here("plots", "VSPG", "01_QC", "MOBP.pdf"), width = 10, height = 10)
+grid.arrange(grobs = plot_list, nrow = 2)
+dev.off()
