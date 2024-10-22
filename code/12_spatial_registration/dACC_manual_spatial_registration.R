a360225a69fcc3b43d9d27b4c7244f1a562a0b98
Kinnary Shah <kinnaryshahh@gmail.com>
2024-10-22 11:41:54 -0400

    dACC manual annotations spatial registration

---
+++ code/12_spatial_registration/dACC_manual_spatial_registration.R
@@ -0,0 +1,41 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+
+library("here")
+library("spatialLIBD")
+library("SingleCellExperiment")
+library("stringr")
+
+k=9
+nnSVG_precast_name <- paste0("nnSVG_PRECAST_captureArea_", k)
+load(file = here("processed-data", "11_differential_expression", "pseudobulk",
+                 "nnSVG_precast_DE", paste0(nnSVG_precast_name,".Rdata")))
+
+modeling_results_manual <- readRDS(file = here("processed-data", "20_WM_comparisons", "modeling_results_WM_vs_CC.RDS"))
+
+## extract t-statics and rename
+registration_t_stats <- modeling_results_manual$enrichment[, grep("^t_stat", colnames(modeling_results_manual$enrichment))]
+colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_stats))
+
+cor_layer <- layer_stat_cor(
+    stats = registration_t_stats,
+    modeling_results = modeling_results,
+    model_type = "enrichment",
+    top_n = 100
+)
+
+cor_layer
+save(cor_layer, file = here("processed-data", "12_spatial_registration",paste0("dACC_manual",nnSVG_precast_name,".rds")))
+
+pdf(file = here::here("plots", "12_spatial_registration", "dACC_manual",
+                      paste0("dACC_manual_",nnSVG_precast_name,"_heatmap.pdf")), width = 14, height = 14)
+layer_stat_cor_plot(cor_layer, max = max(cor_layer))
+title("dACC manual vs. dACC")
+dev.off()
+
+anno <- annotate_registered_clusters(
+    cor_stats_layer = cor_layer,
+    confidence_threshold = 0.25,
+    cutoff_merge_ratio = 0.25
+)
+
+anno
