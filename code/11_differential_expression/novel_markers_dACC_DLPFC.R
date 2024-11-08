5af619842a48173fb91fa776d21c5560665bfa09
Kinnary Shah <kinnaryshahh@gmail.com>
2024-11-08 15:20:26 -0500

    compare genes by logFC in dACC and DLPFC

---
+++ code/11_differential_expression/novel_markers_dACC_DLPFC.R
@@ -0,0 +1,100 @@
+setwd('/dcs04/lieber/marmaypag/spatialdACC_LIBD4125/spatialdACC/')
+suppressPackageStartupMessages({
+    library("here")
+    library("sessioninfo")
+    library("SpatialExperiment")
+    library("scater")
+    library("spatialLIBD")
+    library("dplyr")
+    library('EnhancedVolcano')
+    library('ggrepel')
+})
+
+nnSVG_precast_name <- paste0("nnSVG_PRECAST_captureArea_", 9)
+load(file = here("processed-data", "11_differential_expression", "pseudobulk", "nnSVG_precast_DE", paste0(nnSVG_precast_name,".Rdata"))
+)
+modeling_results_dACC <- modeling_results
+
+load(file = here("processed-data", "12_spatial_registration",paste0("DLPFC_30_DE",".Rdata")))
+modeling_results_DLPFC <- spe_modeling_results
+
+# intersect genes
+genes_dACC <- rownames(modeling_results_dACC[["enrichment"]])
+genes_DLPFC <- rownames(modeling_results_DLPFC[["enrichment"]])
+genes_intersect <- intersect(genes_dACC, genes_DLPFC)
+dACC_results <- modeling_results_dACC[["enrichment"]][genes_intersect,]
+DLPFC_results <- modeling_results_DLPFC[["enrichment"]][genes_intersect,]
+
+# repeat this in a loop for layers L1, L2, L3, L5
+layers <- c("L1", "L2", "L3", "L5")
+p_list <- list()
+
+for (layer in layers) {
+    genes <- dACC_results[, "gene"]
+    dACC_logFC <- dACC_results[, paste0("logFC_", layer)]
+    DLPFC_logFC <- DLPFC_results[, paste0("logFC_", layer)]
+    corr_layer <- cor.test(dACC_logFC, DLPFC_logFC)
+
+    # create data frame
+    df_layer <- data.frame(
+        gene = genes,
+        dACC_logFC = dACC_logFC,
+        DLPFC_logFC = DLPFC_logFC
+    )
+
+    # plot
+    p <- ggplot(df_layer, aes(x = dACC_logFC, y = DLPFC_logFC)) +
+        geom_point(color = "grey") +
+        geom_text_repel(aes(label = gene), size = 3) +
+        geom_smooth(method = "lm", se = FALSE, color = "blue") +
+        geom_vline(xintercept = 0, color = "black", linetype = "dashed") +
+        geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+        xlim(-5, 5) +
+        ylim(-5, 5) +
+        labs(
+            title = paste0("Layer: ", layer),
+            x = "dACC logFC",
+            y = "DLPFC logFC",
+            subtitle = paste0("Pearson's r: ", round(corr_layer$estimate, 2))
+        )
+    p_list[[layer]] <- p
+}
+
+layers <- c("L6a", "L6b")
+for (layer in layers) {
+    genes <- dACC_results[, "gene"]
+    dACC_logFC <- dACC_results[, paste0("logFC_", layer)]
+    DLPFC_logFC <- DLPFC_results[, paste0("logFC_", "L6")]
+    corr_layer <- cor.test(dACC_logFC, DLPFC_logFC)
+
+    # create data frame
+    df_layer <- data.frame(
+        gene = genes,
+        dACC_logFC = dACC_logFC,
+        DLPFC_logFC = DLPFC_logFC
+    )
+
+    # plot
+    p <- ggplot(df_layer, aes(x = dACC_logFC, y = DLPFC_logFC)) +
+        geom_point(color = "grey") +
+        geom_text_repel(aes(label = gene), size = 3) +
+        geom_smooth(method = "lm", se = FALSE, color = "blue") +
+        geom_vline(xintercept = 0, color = "black", linetype = "dashed") +
+        geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+        xlim(-5, 5) +
+        ylim(-5, 5) +
+        labs(
+            title = paste0("DLPFC: L6 & dACC Layer: ", layer),
+            x = "dACC logFC",
+            y = "DLPFC logFC",
+            subtitle = paste0("Pearson's r: ", round(corr_layer$estimate, 2))
+        )
+    p_list[[layer]] <- p
+}
+
+pdf(file = here("plots", "11_differential_expression", "layer_markers_dACC_DLPFC.pdf"), width = 10, height = 10)
+for (layer in names(p_list)) {
+    print(p_list[[layer]])
+}
+dev.off()
+
