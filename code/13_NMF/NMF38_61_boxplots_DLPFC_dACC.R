b019a0e71a9438e14e384b22adf922e7ca2e8a9a
Kinnary Shah <kinnaryshahh@gmail.com>
2025-01-22 10:38:36 -0500

    boxplots of fraction nonzero L5 spots for 38 and 61

--- code/13_NMF/NMF38_61_boxplots_DLPFC_dACC.R
+++ code/13_NMF/NMF38_61_boxplots_DLPFC_dACC.R
@@ -37,7 +37,10 @@ dat_DLPFC <- as.data.frame(colData(spe_DLPFC))
 dat_dACC <- dat_dACC[which(dat_dACC$layer == "L5"),]
 summary_dACC <- dat_dACC %>%
     group_by(brnum) %>%
-    summarize(avg38_dACC = mean(nmf38), avg61_dACC = mean(nmf61))
+    summarize(totalL5_dACC = n(), count38_dACC = sum(nmf38 > 0), count61_dACC = sum(nmf61 > 0))
+
+summary_dACC$frac38_dACC <- summary_dACC$count38_dACC / summary_dACC$totalL5_dACC
+summary_dACC$frac61_dACC <- summary_dACC$count61_dACC / summary_dACC$totalL5_dACC
 
 summary_dACC$region <- rep("dACC", 10)
 
@@ -45,22 +48,26 @@ summary_dACC$region <- rep("dACC", 10)
 dat_DLPFC <- dat_DLPFC[which(dat_DLPFC$BayesSpace_harmony_09 == 4),]
 summary_DLPFC <- dat_DLPFC %>%
     group_by(subject) %>%
-    summarize(avg38_DLPFC = mean(nmf38), avg61_DLPFC = mean(nmf61))
+    summarize(totalL5_DLPFC = n(), count38_DLPFC = sum(nmf38 > 0), count61_DLPFC = sum(nmf61 > 0))
+
+summary_DLPFC$frac38_DLPFC <- summary_DLPFC$count38_DLPFC / summary_DLPFC$totalL5_DLPFC
+summary_DLPFC$frac61_DLPFC <- summary_DLPFC$count61_DLPFC / summary_DLPFC$totalL5_DLPFC
+
 
 summary_DLPFC$region <- rep("DLPFC", 10)
 
 summary_overall <- summary_dACC
 summary_overall[c(11:20),] <- summary_DLPFC
 
-p1 <- ggplot(summary_overall, aes(x=region, y=avg38_dACC), fill=region) +
+p1 <- ggplot(summary_overall, aes(x=region, y=frac38_dACC), fill=region) +
     geom_boxplot() +
-    ylim(c(0,0.0002)) +
-    ggtitle("Average NMF38 Weights by Region")
+    ylim(c(0,0.4)) +
+    ggtitle("Fraction Nonzero NMF38 Spots by Region")
 
-p2 <- ggplot(summary_overall, aes(x=region, y=avg61_dACC), fill=region) +
+p2 <- ggplot(summary_overall, aes(x=region, y=frac61_dACC), fill=region) +
     geom_boxplot() +
-    ylim(c(0,0.0002)) +
-    ggtitle("Average NMF61 Weights by Region")
+    ylim(c(0,0.4)) +
+    ggtitle("Fraction Nonzero NMF61 Spots by Region")
 
 pdf(file = here::here("plots", "13_NMF", "NMF38_61_boxplots_DLPFC_dACC.pdf"))
 print(p1)
