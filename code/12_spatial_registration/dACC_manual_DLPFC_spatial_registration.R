0860e16bc18fdb23b3434bbd8189df9fdedc60ee
Kinnary Shah <kinnaryshahh@gmail.com>
2025-01-24 11:40:25 -0500

    add dACC manual to DLPFC reg, increase font size

--- code/12_spatial_registration/dACC_manual_DLPFC_spatial_registration.R
+++ code/12_spatial_registration/dACC_manual_DLPFC_spatial_registration.R
@@ -5,20 +5,17 @@ library("spatialLIBD")
 library("SingleCellExperiment")
 library("stringr")
 
-k=9
-nnSVG_precast_name <- paste0("nnSVG_PRECAST_captureArea_", k)
-load(file = here("processed-data", "11_differential_expression", "pseudobulk",
-                 "nnSVG_precast_DE", paste0(nnSVG_precast_name,".Rdata")))
-
 modeling_results_manual <- readRDS(file = here("processed-data", "20_WM_comparisons", "modeling_results_WM_vs_CC.RDS"))
 
 ## extract t-statics and rename
 registration_t_stats <- modeling_results_manual$enrichment[, grep("^t_stat", colnames(modeling_results_manual$enrichment))]
 colnames(registration_t_stats) <- gsub("^t_stat_", "", colnames(registration_t_stats))
 
+load(file = here("processed-data", "12_spatial_registration",paste0("DLPFC_30_DE",".Rdata")))
+
 cor_layer <- layer_stat_cor(
     stats = registration_t_stats,
-    modeling_results = modeling_results,
+    modeling_results = spe_modeling_results,
     model_type = "enrichment",
     top_n = 100
 )
@@ -27,7 +24,7 @@ cor_layer
 save(cor_layer, file = here("processed-data", "12_spatial_registration",paste0("dACC_manual",nnSVG_precast_name,".rds")))
 
 pdf(file = here::here("plots", "12_spatial_registration", "dACC_manual",
-                      paste0("dACC_manual_",nnSVG_precast_name,"_heatmap.pdf")), width = 5, height = 5)
+                      paste0("dACC_manual_","DLPFC_30","_heatmap.pdf")), width = 5, height = 5)
 layer_stat_cor_plot(cor_layer, max = max(cor_layer))
 title("")
 dev.off()
