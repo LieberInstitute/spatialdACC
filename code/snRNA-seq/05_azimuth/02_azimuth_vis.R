59c0daeb5a30a55218c686321cacf068a44e6a97
Kinnary Shah <kinnaryshahh@gmail.com>
2025-01-28 12:23:23 -0500

    vis with floating labels

--- code/snRNA-seq/05_azimuth/02_azimuth_vis.R
+++ code/snRNA-seq/05_azimuth/02_azimuth_vis.R
@@ -16,13 +16,42 @@ library("svglite")
 load(file = here("processed-data", "snRNA-seq", "05_azimuth", "sce_azimuth.Rdata"))
 
 #plot harmony UMAP colored by azimuth labels
-sce$cellType_azimuth <- as.factor(sce$cellType_azimuth)
-p <- plotReducedDim(sce, dimred="UMAP-HARMONY", colour_by="cellType_azimuth", point_size = 0.5) +
-    xlab("UMAP 1") +
-    ylab("UMAP 2") +
-    labs(color = "Cell Type")
 
-ggsave(p, file = here::here("plots", "snRNA-seq", "05_azimuth", "HARMONY_azimuth_UMAP.pdf"), useDingbats = FALSE)
+# remove Sst Chodl
+sce <- sce[,-which(sce$cellType_azimuth == "Sst Chodl")]
+sce$cellType_azimuth <- as.factor(sce$cellType_azimuth)
+
+mycolors <- pals::cols25()[1:19]
+celltype_colors <- setNames(mycolors, unique(sce$fine_celltype))
+celltype_colors
+
+umap_coords <- reducedDim(sce, "UMAP-HARMONY")
+plot_data <- data.frame(umap_coords, Celltype = sce$cellType_azimuth)
+
+# Calculate centroids for each group
+centroid_data <- plot_data %>%
+    group_by(Celltype) %>%
+    summarize(CentroidX = mean(UMAP1), CentroidY = mean(UMAP2), .groups = 'keep')
+
+# Match colors to annotations in centroid_data
+centroid_data$Color <- celltype_colors[centroid_data$Celltype]
+
+
+p1 <- plotReducedDim(sce, dimred = "UMAP-HARMONY", colour_by = "cellType_azimuth", point_size=0.3) +
+    scale_color_manual(values = celltype_colors) +
+    ggrepel::geom_label_repel(data = centroid_data, aes(x = CentroidX, y = CentroidY, label = Celltype),
+                              #color = centroid_data$Color,
+                              fontface = "bold.italic", size=3) +
+    theme(axis.line=element_blank(),axis.text.x=element_blank(),
+          axis.text.y=element_blank(),axis.ticks=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank(),legend.position="none",
+          panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),plot.background=element_blank())
+
+pdf(file = here::here("plots", "snRNA-seq", "05_azimuth", "HARMONY_azimuth_UMAP.pdf"), height = 6, width = 6)
+p1
+dev.off()
 
 p1 <- plotReducedDim(sce, dimred="TSNE-HARMONY", colour_by="cellType_azimuth", point_size = 0.5) +
     xlab("t-SNE 1") +
