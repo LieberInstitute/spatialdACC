7227dedc355d1ac3f9e65b404209d9c26ed0a114
Kinnary Shah <kinnaryshahh@gmail.com>
2025-02-10 17:08:36 -0500

    add colors to heatmap

--- code/12_spatial_registration/dACC_manual_spatial_registration.R
+++ code/12_spatial_registration/dACC_manual_spatial_registration.R
@@ -28,10 +28,22 @@ save(cor_layer, file = here("processed-data", "12_spatial_registration",paste0("
 
 cor_layer <- cor_layer[c(6,7,8,4,5,3,2,1),]
 
+layer_colors <- c(
+    "L2" = "#377EB8",
+    "L3" = "#4DAF4A",
+    "L5" = "#FFD700",
+    "L6b" = "#c46200",
+    "L6a" = "#FFC18A",
+    "WM" = "#1A1A1A",
+    "L1" = "#F0027F"
+)
+
 pdf(file = here::here("plots", "12_spatial_registration", "dACC_manual",
-                      paste0("dACC_manual_",nnSVG_precast_name,"_heatmap.pdf")), width = 5, height = 5)
-layer_stat_cor_plot(cor_layer, max = max(cor_layer))
-title("Manual vs. PRECAST")
+                      paste0("dACC_manual_",nnSVG_precast_name,"_heatmap.pdf")), width = 6, height = 4)
+layer_stat_cor_plot(t(cor_layer), color_max = max(cor_layer),
+                    query_colors = layer_colors,
+                    cluster_rows = FALSE,
+                    cluster_columns = FALSE)
 dev.off()
 
 anno <- annotate_registered_clusters(
