7c42c86f3d6ee3f206f10d3efc431ea741ed2977
kinnaryshah <kinnaryshahh@gmail.com>
2025-04-14 11:53:42 -0400

    update file

--- code/15_cross_region_snRNA-seq/01_cross_validation.R
+++ code/15_cross_region_snRNA-seq/01_cross_validation.R
@@ -18,9 +18,29 @@ sce <- HDF5Array::loadHDF5SummarizedExperiment(
 
 assay(sce, "logcounts") <- as(assay(sce, "logcounts"), "dgCMatrix")
 
+#cvnmf <- cross_validate_nmf(
+#    logcounts(sce),
+#    ranks=c(25,50,75,100,125),
+#    n_replicates = 2,
+#    tol = 1e-03,
+#    maxit = 100,
+#    verbose = 3,
+#    L1 = 0.1,
+#    L2 = 0,
+#    threads = 0,
+#    test_density = 0.2
+#)
+
+#saveRDS(cvnmf, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.RDS"))
+
+#pdf(here("plots", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.pdf"))
+#plot(cvnmf)
+#dev.off()
+
+set.seed(78)
 cvnmf <- cross_validate_nmf(
     logcounts(sce),
-    ranks=c(25,50,75,100,125),
+    ranks=c(5, 10, 15, 20, 25, 30, 50, 75, 100),
     n_replicates = 2,
     tol = 1e-03,
     maxit = 100,
@@ -31,9 +51,8 @@ cvnmf <- cross_validate_nmf(
     test_density = 0.2
 )
 
-saveRDS(cvnmf, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.RDS"))
-
-pdf(here("plots", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results.pdf"))
+pdf(here("plots", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results_lower_k.pdf"))
 plot(cvnmf)
 dev.off()
 
+saveRDS(cvnmf, file = here("processed-data", "15_cross_region_snRNA-seq", "DLPFC_nmf_cv_results_lower_k.RDS"))
