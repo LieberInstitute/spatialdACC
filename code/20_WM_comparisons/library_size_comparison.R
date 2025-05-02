608570ce47901ee82428a7af04904425d030c6d2
Kinnary Shah <kinnaryshahh@gmail.com>
2025-05-02 09:03:56 -0400

    vis WM library size

--- code/20_WM_comparisons/library_size_comparison.R
+++ code/20_WM_comparisons/library_size_comparison.R
@@ -10,7 +10,7 @@ library(here)
 library(scran)
 library(scry)
 
-load(here("processed-data", "08_clustering", "PRECAST", "spe_nnSVG_PRECAST_9_labels.Rdata"))
+load(here("processed-data", "08_clustering", "PRECAST", "spe_nnSVG_PRECAST_9.Rdata"))
 
 # compare library size of WM1, WM2, WM-CC
 # overall and separately by sample
@@ -19,7 +19,7 @@ load(here("processed-data", "08_clustering", "PRECAST", "spe_nnSVG_PRECAST_9_lab
 # create a dataframe by taking the average of colData(spe)$sizeFactor for each sample in colData(spe)$sample_id and cluster in colData(spe)$PRECAST_cluster
 
 # Filter for relevant clusters
-wm_clusters <- c("WM1", "WM2", "WM-CC")
+wm_clusters <- c("3", "8", "7")
 
 # Create a dataframe with the average library size (sizeFactor) for each sample and cluster
 df_avg_library_size <- colData(spe) %>%
@@ -31,13 +31,13 @@ df_avg_library_size <- colData(spe) %>%
 # View the resulting dataframe
 head(df_avg_library_size)
 
-pdf(here("plots", "20_WM_comparisons", "sizeFactor_comparison.pdf"), width = 20, height = 5)
+png(here("plots", "20_WM_comparisons", "sizeFactor_comparison.png"), unit="in", res=300, width = 7, height = 5)
 ggplot(df_avg_library_size, aes(x = sample_id, y = avg_library_size, color = PRECAST_cluster)) +
     geom_point(size = 3) +
-    # make x axis labels vertical
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    scale_color_manual(values = c("3"="blue","7"="brown","8"="grey"), name="PRECAST Cluster") +
     theme_bw() +
-    labs(title = "Average Library Size by Sample and Cluster (WM1, WM2, WM-CC)",
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    labs(title = "Average Library Size by Sample and Cluster (WM regions)",
          x = "Sample ID",
          y = "Average Library Size")
 dev.off()
