966662d78b6682bbf8422b421fcb26c16e163e17
Kinnary Shah <kinnaryshahh@gmail.com>
2025-05-14 16:10:39 -0400

    NMF38 and 61 quantifications in SRT and snRNA-seq

--- code/13_NMF/NMF38_61_boxplots_DLPFC_dACC.R
+++ code/13_NMF/NMF38_61_boxplots_DLPFC_dACC.R
@@ -66,7 +66,6 @@ p1 <- ggplot(summary_overall, aes(x=region, y=frac38_dACC)) +
     ggtitle("") +
     theme_bw()
 
-
 p2 <- ggplot(summary_overall, aes(x=region, y=frac61_dACC)) +
     geom_boxplot(outlier.shape = NA, color="#FFD700") +
     geom_point(color="#FFD700", size=1, alpha=0.8) +
@@ -75,38 +74,136 @@ p2 <- ggplot(summary_overall, aes(x=region, y=frac61_dACC)) +
     ggtitle("") +
     theme_bw()
 
+# also calculate average weights for SRT data
+dat_dACC <- as.data.frame(colData(spe_dACC))
+dat_DLPFC <- as.data.frame(colData(spe_DLPFC))
+
+dat_DLPFC <- dat_DLPFC[which(dat_DLPFC$BayesSpace_harmony_09 == 4),]
+summary_DLPFC <- dat_DLPFC %>%
+    group_by(subject) %>%
+    summarize(avg38 = mean(nmf38), avg61 = mean(nmf61))
+
+summary_DLPFC$region <- rep("dlPFC", 10)
+
+dat_dACC <- dat_dACC[which(dat_dACC$layer == "L5"),]
+summary_dACC <- dat_dACC %>%
+    group_by(brnum) %>%
+    summarize(avg38 = mean(nmf38), avg61 = mean(nmf61))
+
+summary_dACC$region <- rep("dACC", 10)
+
+summary_overall <- summary_dACC
+summary_overall[c(11:20),] <- summary_DLPFC
+
+
+p3 <- ggplot(summary_overall, aes(x=region, y=avg38, color=region)) +
+    geom_boxplot(outlier.shape = NA) +
+    ylim(c(0,0.00011)) +
+    geom_point(size=1, alpha=0.8) +
+    ylab("Avg. NMF38 Weight") +
+    ggtitle("") +
+    xlab("region") +
+    theme_bw() +
+    scale_color_manual(values = c("#FFD700", "#FFD700")) +
+    theme(legend.position="none")
+
+p4 <- ggplot(summary_overall, aes(x=region, y=avg61, color=region)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point(size=1, alpha=0.8) +
+    ylab("Avg. NMF61 Weight") +
+    ylim(c(0.000,0.000185)) +
+    ggtitle("") +
+    xlab("region") +
+    theme_bw() +
+    scale_color_manual(values = c("#FFD700", "#FFD700")) +
+    theme(legend.position="none")
+
+
+# calculate nonzero spots for snRNA-seq data
 load(file = here("processed-data", "13_NMF", "DLPFC_dACC_celltype_NMF.Rdata"))
-# we do not use the Azimuth annotations because they are not good for dlPFC
 
-dat_DLPFC <- dat_DLPFC[which(sce_DLPFC$cellType_layer %in% c("Excit_L5")),]
-summary_DLPFC_IT <- dat_DLPFC %>%
-    group_by(Sample) %>%
-    summarize(avg38 = mean(nmf38), avg61 = mean(nmf61))
+dat_DLPFC_orig <- dat_DLPFC
 
-summary_DLPFC_IT$region <- rep("dlPFC", dim(summary_DLPFC_IT)[1])
-summary_DLPFC_IT$celltype <- rep("Excit_L5", dim(summary_DLPFC_IT)[1])
+dat_DLPFC <- dat_DLPFC[which(dat_DLPFC$cellType_layer %in% c("Excit_L5")),]
+summary_DLPFC <- dat_DLPFC %>%
+    group_by(BrNum) %>%
+    summarize(totalL5_DLPFC = n(), count38_DLPFC = sum(nmf38 > 0), count61_DLPFC = sum(nmf61 > 0))
 
-dat_DLPFC <- as.data.frame(colData(sce_DLPFC))
-dat_DLPFC <- dat_DLPFC[which(sce_DLPFC$cellType_layer %in% c("Excit_L5")),]
-summary_DLPFC_ET <- dat_DLPFC %>%
-    group_by(Sample) %>%
-    summarize(avg38 = mean(nmf38), avg61 = mean(nmf61))
+summary_DLPFC$region <- rep("dlPFC", dim(summary_DLPFC)[1])
+summary_DLPFC$celltype <- rep("Excit_L5", dim(summary_DLPFC)[1])
 
-summary_DLPFC_ET$region <- rep("dlPFC", dim(summary_DLPFC_ET)[1])
-summary_DLPFC_ET$celltype <- rep("Excit_L5", dim(summary_DLPFC_ET)[1])
-
-dat_dACC <- dat_dACC[which(dat_dACC$cellType_layer %in% c("Excit_L5")),]
-summary_dACC_IT <- dat_dACC %>%
-    group_by(Sample) %>%
-    summarize(avg38 = mean(NMF_38), avg61 = mean(NMF_61))
-
-summary_dACC_IT$region <- rep("dACC", 10)
-summary_dACC_IT$celltype <- rep("Excit_L5", 10)
+summary_DLPFC$frac38_DLPFC <- summary_DLPFC$count38_DLPFC / summary_DLPFC$totalL5_DLPFC
+summary_DLPFC$frac61_DLPFC <- summary_DLPFC$count61_DLPFC / summary_DLPFC$totalL5_DLPFC
 
 dat_dACC_orig <- dat_dACC
 dat_dACC <- dat_dACC[which(dat_dACC$cellType_azimuth %in% c("L5_IT")),]
 summary_dACC_IT <- dat_dACC %>%
-    group_by(Sample) %>%
+    group_by(brain) %>%
+    summarize(totalL5_dACC = n(), count38_dACC = sum(NMF_38 > 0), count61_dACC = sum(NMF_61 > 0))
+
+summary_dACC_IT$region <- rep("dACC", 10)
+summary_dACC_IT$celltype <- rep("L5_IT", 10)
+
+summary_dACC_IT$frac38_dACC <- summary_dACC_IT$count38_dACC / summary_dACC_IT$totalL5_dACC
+summary_dACC_IT$frac61_dACC <- summary_dACC_IT$count61_dACC / summary_dACC_IT$totalL5_dACC
+
+
+dat_dACC <- dat_dACC_orig
+dat_dACC <- dat_dACC[which(dat_dACC$cellType_azimuth %in% c("L5_ET")),]
+summary_dACC_ET <- dat_dACC %>%
+    group_by(brain) %>%
+    summarize(totalL5_dACC = n(), count38_dACC = sum(NMF_38 > 0), count61_dACC = sum(NMF_61 > 0))
+
+summary_dACC_ET$region <- rep("dACC", 10)
+summary_dACC_ET$celltype <- rep("L5_ET", 10)
+
+summary_dACC_ET$frac38_dACC <- summary_dACC_ET$count38_dACC / summary_dACC_ET$totalL5_dACC
+summary_dACC_ET$frac61_dACC <- summary_dACC_ET$count61_dACC / summary_dACC_ET$totalL5_dACC
+
+summary_overall <- summary_dACC_IT
+summary_overall[c(11:20),] <- summary_DLPFC
+
+p5 <- ggplot(summary_overall, aes(x=region, y=frac38_dACC, color=region)) +
+    geom_boxplot(outlier.shape = NA) +
+    ylim(c(0,1)) +
+    geom_point(size=1, alpha=0.8) +
+    ylab("Frac. Nonzero NMF38") +
+    scale_color_manual(values = c("#6A33C2", "black")) +
+    ggtitle("") +
+    theme_bw() +
+    theme(legend.position="none")
+
+summary_overall <- summary_dACC_ET
+summary_overall[c(11:20),] <- summary_DLPFC
+
+p6 <- ggplot(summary_overall, aes(x=region, y=frac61_dACC, color=region)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_point(size=1, alpha=0.8) +
+    ylab("Frac. Nonzero NMF61") +
+    ylim(c(0,1)) +
+    ggtitle("") +
+    theme_bw() +
+    scale_color_manual(values = c("#C8308C", "black")) +
+    theme(legend.position="none")
+
+
+# calculate average weights for snRNA-seq data
+load(file = here("processed-data", "13_NMF", "DLPFC_dACC_celltype_NMF.Rdata"))
+
+dat_DLPFC_orig <- dat_DLPFC
+
+dat_DLPFC <- dat_DLPFC[which(dat_DLPFC$cellType_layer %in% c("Excit_L5")),]
+summary_DLPFC <- dat_DLPFC %>%
+    group_by(BrNum) %>%
+    summarize(avg38 = mean(nmf38), avg61 = mean(nmf61))
+
+summary_DLPFC$region <- rep("dlPFC", dim(summary_DLPFC)[1])
+summary_DLPFC$celltype <- rep("Excit_L5", dim(summary_DLPFC)[1])
+
+dat_dACC_orig <- dat_dACC
+dat_dACC <- dat_dACC[which(dat_dACC$cellType_azimuth %in% c("L5_IT")),]
+summary_dACC_IT <- dat_dACC %>%
+    group_by(brain) %>%
     summarize(avg38 = mean(NMF_38), avg61 = mean(NMF_61))
 
 summary_dACC_IT$region <- rep("dACC", 10)
@@ -115,17 +212,16 @@ summary_dACC_IT$celltype <- rep("L5_IT", 10)
 dat_dACC <- dat_dACC_orig
 dat_dACC <- dat_dACC[which(dat_dACC$cellType_azimuth %in% c("L5_ET")),]
 summary_dACC_ET <- dat_dACC %>%
-    group_by(Sample) %>%
+    group_by(brain) %>%
     summarize(avg38 = mean(NMF_38), avg61 = mean(NMF_61))
 
 summary_dACC_ET$region <- rep("dACC", 10)
 summary_dACC_ET$celltype <- rep("L5_ET", 10)
 
 summary_overall <- summary_dACC_IT
-summary_overall[c(11:28),] <- summary_DLPFC_ET
+summary_overall[c(11:20),] <- summary_DLPFC
 
-
-p3 <- ggplot(summary_overall, aes(x=region, y=avg38, color=region)) +
+p7 <- ggplot(summary_overall, aes(x=region, y=avg38, color=region)) +
     geom_boxplot(outlier.shape = NA) +
     ylim(c(0,0.00065)) +
     geom_point(size=1, alpha=0.8) +
@@ -137,9 +233,9 @@ p3 <- ggplot(summary_overall, aes(x=region, y=avg38, color=region)) +
     theme(legend.position="none")
 
 summary_overall <- summary_dACC_ET
-summary_overall[c(11:28),] <- summary_DLPFC_ET
+summary_overall[c(11:20),] <- summary_DLPFC
 
-p4 <- ggplot(summary_overall, aes(x=region, y=avg61, color=region)) +
+p8 <- ggplot(summary_overall, aes(x=region, y=avg61, color=region)) +
     geom_boxplot(outlier.shape = NA) +
     geom_point(size=1, alpha=0.8) +
     ylab("Avg. NMF61 Weight") +
@@ -152,8 +248,9 @@ p4 <- ggplot(summary_overall, aes(x=region, y=avg61, color=region)) +
 
 
 pdf(file = here::here("plots", "13_NMF", "NMF_boxplots_DLPFC_dACC.pdf"), height = 6, width = 6)
-wrap_plots(list(p1,p3,p2,p4), nrow = 2)
+wrap_plots(list(p1,p3,p2,p4), nrow = 2) + plot_annotation(title = "NMF38 and 61 in SRT",
+                                                          caption = "Layer 5 in dACC and dlPFC")
+wrap_plots(list(p5,p7,p6,p8), nrow = 2) + plot_annotation(title = "NMF38 and 61 in snRNA-seq",
+                                                          caption = "L5 in dlPFC; L5 IT in dACC (top), L5 ET in dACC (bottom)")
 dev.off()
-
-
 
