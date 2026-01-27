f4e864eead5badfc62da0838a713998c46798ed3
Kinnary Shah <kinnaryshahh@gmail.com>
2026-01-27 11:49:43 -0500

    run RCTD on dlPFC without Ambig, L345

---
+++ code/22_deconvolution/RCTD_dlPFC_snRNA-seq_dlPFC_SRT_noExcitAmbig.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=rctd
+#SBATCH --ntasks=1                 # Run on a single CPU
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=30         # Number of CPU cores per task
+#SBATCH --mail-type=END
+#SBATCH --mail-user=kinnaryshahh@gmail.com
+#SBATCH --time=1-00:00:00
+
+echo "**** Job starts ****"
+date
+echo "**** SLURM info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+module load conda_R/4.5.x
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript RCTD_dlPFC_snRNA-seq_dlPFC_SRT_noExcitAmbig.R
+
+echo "**** Job ends ****"
+date
+
